4a68b3c9dac9e5ea271e1db28a74775bcb4d6d17
Audrey Dutcher <audrey@rhelmot.io>
2021-12-31 17:27:53 -0800

    Second draft of radio buttons

--- core/src/views/radio_buttons.rs
+++ core/src/views/radio_buttons.rs
@@ -1,23 +1,16 @@
-use std::collections::HashMap;
-use crate::{Context, Entity, Event, Handle, View, WindowEvent, MouseButton, PseudoClass, Label, LayoutType, HStack, VStack, TreeExt};
+use crate::{Context, Entity, Event, Handle, View, WindowEvent, MouseButton, PseudoClass, Label, LayoutType};
 use crate::style::PropSet;
 use crate::Units::*;
 
 pub struct RadioButtons {
-    pub idx: usize,
     on_changed: Option<Box<dyn Fn(&mut Context, usize)>>,
-    entity_map: HashMap<Entity, usize>,
-    check_boxes: Vec<Entity>,
-    icon_checked: String,
-    icon_unchecked: String,
 }
 
-const ICON_CHECK: &str = "\u{2713}";
+const ICON_CHECK: &str = "\u{00B7}";
 
 impl RadioButtons {
     pub fn with_icons<F>(
         cx: &mut Context,
-        layout: LayoutType,
         idx: usize,
         idx_count: usize,
         builder: F,
@@ -27,49 +20,23 @@ impl RadioButtons {
     where
         F: 'static + Fn(&mut Context, usize) + Clone
     {
-        let result = Self {
-            idx,
+        Self {
             on_changed: None,
-            icon_checked, icon_unchecked,
-            entity_map: HashMap::new(),
-            check_boxes: vec![],
         }
             .build2(cx, move |cx| {
-                let top_entity = cx.current;
-                let stack_builder = move |cx: &mut Context| {
-                    for available_idx in 0..idx_count {
-                        let one_builder = builder.clone();
-                        HStack::new(cx, move |cx| {
-                            let hstack_id = cx.current;
-                            let view = downcast_view_mut(cx, top_entity).unwrap();
-                            let lbl_txt = if available_idx == idx { &view.icon_checked } else { &view.icon_unchecked }.clone();
-                            let checkbox = Label::new(cx, &lbl_txt)
-                                .class("radioknob");
-                            downcast_view_mut(cx, top_entity).unwrap().check_boxes.push(checkbox.entity);
-
-                            (one_builder)(cx, available_idx);
-
-                            let children = hstack_id.branch_iter(&cx.tree).collect::<Vec<_>>();
-                            let view = downcast_view_mut(cx, top_entity).unwrap();
-                            for child in children {
-                                view.entity_map.insert(child, available_idx);
-                            }
-                        });
-                    }
-                };
-                match layout {
-                    LayoutType::Row => { HStack::new(cx, stack_builder); }
-                    LayoutType::Column => { VStack::new(cx, stack_builder); }
-                    LayoutType::Grid => panic!("Why do you want a grid RadioButtons? go away"),
-                };
+                for idx_available in 0..idx_count {
+                    let checked = idx_available == idx;
+                    let text = if checked { icon_checked.clone() } else { icon_unchecked.clone() };
+                    let one_builder = builder.clone();
+                    RadioButton::new(cx, idx_available, text, move |cx| {
+                        (one_builder)(cx, idx_available);
+                    });
+                }
             })
-            .height(Pixels(20.0));
-        result
     }
 
     pub fn new<F>(
         cx: &mut Context,
-        layout: LayoutType,
         idx: usize,
         idx_count: usize,
         builder: F
@@ -77,7 +44,7 @@ impl RadioButtons {
         where
             F: 'static + Fn(&mut Context, usize) + Clone
     {
-        Self::with_icons(cx, layout, idx, idx_count, builder, ICON_CHECK.to_owned(), "".to_owned())
+        Self::with_icons(cx, idx, idx_count, builder, ICON_CHECK.to_owned(), "".to_owned())
     }
 }
 
@@ -86,19 +53,14 @@ impl Handle<RadioButtons> {
         where
             F: 'static + Fn(&mut Context, usize),
     {
-        downcast_view_mut(cx, self.entity).unwrap().on_changed = Some(Box::new(callback));
+        if let Some(view) = cx.views.get_mut(&self.entity) {
+            if let Some(view) = view.downcast_mut::<RadioButtons>() {
+                view.on_changed = Some(Box::new(callback));
+            }
+        }
         self
     }
 
-}
-
-pub fn downcast_view_mut(cx: &mut Context, entity: Entity) -> Option<&mut RadioButtons>
-{
-    if let Some(view) = cx.views.get_mut(&entity) {
-        view.downcast_mut::<RadioButtons>()
-    } else {
-        None
-    }
 }
 
 impl View for RadioButtons {
@@ -107,33 +69,50 @@ impl View for RadioButtons {
     }
 
     fn event(&mut self, cx: &mut Context, event: &mut Event) {
-        if let Some(window_event) = event.message.downcast() {
-            match window_event {
-                WindowEvent::MouseDown(button) if *button == MouseButton::Left => {
-                    if let Some(idx) = self.entity_map.get(&event.target) {
-                        if *idx != self.idx {
-                            let old_check = self.check_boxes[self.idx];
-                            if let Some(pseudo_classes) = cx.style.borrow_mut().pseudo_classes.get_mut(old_check) {
-                                pseudo_classes.set(PseudoClass::CHECKED, false);
-                            }
-                            old_check.set_text(cx, &self.icon_unchecked);
-
-                            self.idx = *idx;
-                            if let Some(func) = self.on_changed.take() {
-                                func(cx, *idx);
-                                self.on_changed = Some(func);
-                            }
-
-                            let new_check = self.check_boxes[self.idx];
-                            if let Some(pseudo_classes) = cx.style.borrow_mut().pseudo_classes.get_mut(new_check) {
-                                pseudo_classes.set(PseudoClass::CHECKED, true);
-                            }
-                            new_check.set_text(cx, &self.icon_checked);
-                        }
-                    }
-                }
-                _ => {}
+        if let Some(RadioButtonSelected(idx)) = event.message.downcast() {
+            if let Some(func) = self.on_changed.take() {
+                (func)(cx, *idx);
+                self.on_changed = Some(func);
             }
         }
     }
 }
+
+struct RadioButton {
+    idx: usize,
+}
+
+impl View for RadioButton {
+    fn element(&self) -> Option<String> {
+        Some("radiobutton".to_owned())
+    }
+
+    fn event(&mut self, cx: &mut Context, event: &mut Event) {
+        if let Some(WindowEvent::MouseDown(btn)) = event.message.downcast() {
+            if *btn == MouseButton::Left {
+                cx.emit(RadioButtonSelected(self.idx));
+            }
+        }
+    }
+}
+
+impl RadioButton {
+    fn new<F>(cx: &mut Context, idx: usize, icon_text: String, builder: F) -> Handle<RadioButton>
+    where
+        F: 'static + FnOnce(&mut Context)
+    {
+        RadioButton {
+            idx
+        }.build2(cx, move |cx| {
+            let entity = cx.current;
+            entity.set_layout_type(cx, LayoutType::Row);
+            entity.set_height(cx, Stretch(0.0));
+            Label::new(cx, &icon_text)
+                .class("radioknob");
+            (builder)(cx);
+        })
+    }
+}
+
+#[derive(Debug)]
+struct RadioButtonSelected(usize);
