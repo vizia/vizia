3cc4e97c8a7adb746edf6a58654ebf739d4b44f5
Roope Salmi <rpsalmi@gmail.com>
2022-02-10 04:02:24 +0200

    Test TreeIterator

--- core/src/tree/tree_iter.rs
+++ core/src/tree/tree_iter.rs
@@ -34,3 +34,51 @@ impl<'a> DoubleEndedIterator for TreeIterator<'a> {
         })
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::TreeError;
+
+    #[test]
+    fn simple_forward_backward() -> Result<(), TreeError> {
+        let mut t = Tree::new();
+        let r = Entity::root();
+        let [a, b, c, d, e] = [1, 2, 3, 4, 5].map(|i| Entity::new(i, 0));
+        t.add(a, r)?;
+        t.add(b, r)?;
+        t.add(c, a)?;
+        t.add(d, a)?;
+        t.add(e, b)?;
+        let correct = [r, a, c, d, b, e];
+        let forward = TreeIterator::full(&t);
+        let backward = TreeIterator::full(&t).rev();
+        assert!(forward.eq(correct.iter().cloned()));
+        assert!(backward.eq(correct.iter().cloned().rev()));
+
+        // correct DoubleEndedIterator behavior, each item yielded only once
+        let mut double = TreeIterator::full(&t);
+        let mut front = Vec::new();
+        let mut back = Vec::new();
+        loop {
+            if let Some(x) = double.next() {
+                front.push(x);
+            }
+            if let Some(x) = double.next_back() {
+                back.push(x);
+            } else {
+                break;
+            }
+        }
+        back.reverse();
+        front.append(&mut back);
+        assert!(front.iter().eq(correct.iter()));
+
+        let correct = [a, c, d];
+        let forward = TreeIterator::subtree(&t, a);
+        let backward = TreeIterator::subtree(&t, a).rev();
+        assert!(forward.eq(correct.iter().cloned()));
+        assert!(backward.eq(correct.iter().cloned().rev()));
+        Ok(())
+    }
+}
