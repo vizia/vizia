cdfc8072301b2fd9bc5903e81134c4224cdcb981
Audrey Dutcher <audrey@rhelmot.io>
2022-02-26 23:05:31 -0700

    the rest of the fmt

--- examples/scrollview.rs
+++ examples/scrollview.rs
@@ -13,23 +13,34 @@ fn main() {
             child_x: 1000.0,
             child_y: 300.0,
             parent_x: 300.0,
-            parent_y: 300.0
-        }.build(cx);
+            parent_y: 300.0,
+        }
+        .build(cx);
         HStack::new(cx, |cx| {
             ScrollView::custom(cx, false, false, ScrollData::root, |cx| {
-                Label::new(cx, "Label 1").width(Units::Pixels(1000.0)).background_color(Color::green());
+                Label::new(cx, "Label 1")
+                    .width(Units::Pixels(1000.0))
+                    .background_color(Color::green());
             })
-                .size(Units::Pixels(300.0));
+            .size(Units::Pixels(300.0));
             ScrollView::custom(cx, false, false, ScrollData::root, |cx| {
-                Label::new(cx, "Label 2").width(Units::Pixels(1000.0)).background_color(Color::blue());
+                Label::new(cx, "Label 2")
+                    .width(Units::Pixels(1000.0))
+                    .background_color(Color::blue());
             })
-                .size(Units::Pixels(300.0));
+            .size(Units::Pixels(300.0));
         });
-        Scrollbar::new(cx, ScrollData::scroll_x, RatioLens::new(ScrollData::parent_x, ScrollData::child_x), Orientation::Horizontal, |cx, scroll| {
-            cx.emit(ScrollUpdate::SetX(scroll));
-        })
-            .width(Units::Pixels(600.0))
-            .height(Units::Pixels(14.0));
+        Scrollbar::new(
+            cx,
+            ScrollData::scroll_x,
+            RatioLens::new(ScrollData::parent_x, ScrollData::child_x),
+            Orientation::Horizontal,
+            |cx, scroll| {
+                cx.emit(ScrollUpdate::SetX(scroll));
+            },
+        )
+        .width(Units::Pixels(600.0))
+        .height(Units::Pixels(14.0));
     })
-        .run();
+    .run();
 }
