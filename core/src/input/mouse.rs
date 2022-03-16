f3f5e539cf4247127c4ac0965729aa048d3128da
George Atkinson <geomyles@yahoo.co.uk>
2022-03-16 23:08:19 +0000

    Add delta function to `MouseState`

--- core/src/input/mouse.rs
+++ core/src/input/mouse.rs
@@ -19,10 +19,15 @@ pub enum MouseButtonState {
 /// Data which describes the current state of a mouse button.
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub struct MouseButtonData {
+    /// The state of the mouse button (pressed/released)
     pub state: MouseButtonState,
+    /// The position of the mouse cursor when the mouse button was last pressed
     pub pos_down: (f32, f32),
+    /// The position of the mouse cursor when the mouse button was last released
     pub pos_up: (f32, f32),
+    /// The hovered entity when the mouse button was last pressed
     pub pressed: Entity,
+    /// The hovered entity when the mouse button was last released
     pub released: Entity,
 }
 
@@ -57,6 +62,26 @@ impl Default for MouseState {
             left: MouseButtonData::default(),
             right: MouseButtonData::default(),
             middle: MouseButtonData::default(),
+        }
+    }
+}
+
+impl MouseState {
+    pub fn delta(&self, button: MouseButton) -> (f32, f32) {
+        match button {
+            MouseButton::Left => {
+                (self.cursorx - self.left.pos_down.0, self.cursory - self.left.pos_down.1)
+            }
+
+            MouseButton::Right => {
+                (self.cursorx - self.right.pos_down.0, self.cursory - self.right.pos_down.1)
+            }
+
+            MouseButton::Middle => {
+                (self.cursorx - self.middle.pos_down.0, self.cursory - self.middle.pos_down.1)
+            }
+
+            _ => (0.0, 0.0),
         }
     }
 }
