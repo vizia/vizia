668f5556e0bc01d673db59505da81471c01e1711
kdecay <kdecaymusic@protonmail.com>
2022-04-04 20:05:48 +0200

    Update label doc string

--- core/src/views/label.rs
+++ core/src/views/label.rs
@@ -1,8 +1,46 @@
 use crate::{Context, Handle, Res, View};
 
+/// A label used to display text to the screen.
+///
+/// # Examples
+///
+/// ## Basic label
+///
+/// A label can be used to simply display some text on the screen.
+///
+/// ```
+/// # use vizia_core::*;
+/// #
+/// # let cx = &mut Context::new();
+/// #
+/// Label::new(cx, "Text");
+/// ```
+///
+/// ## Label for a button
+///
+/// A label can also be used inside of a button to be able to add text to it.
+///
+/// ```
+/// # use vizia_core::*;
+/// #
+/// # let cx = &mut Context::new();
+/// #
+/// Button::new(cx, |_| {}, |cx| Label::new(cx, "Text"));
+/// ```
 pub struct Label;
 
 impl Label {
+    /// Creates a new label.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use vizia_core::*;
+    /// #
+    /// # let cx = &mut Context::new();
+    /// #
+    /// let label = Label::new(cx, "Text");
+    /// ```
     pub fn new<'a, T>(cx: &'a mut Context, text: impl Res<T>) -> Handle<'a, Self>
     where
         T: ToString,
@@ -13,6 +51,6 @@ impl Label {
 
 impl View for Label {
     fn element(&self) -> Option<String> {
-        Some("label".to_string())
+        Some(String::from("label"))
     }
 }
