54712b177ff2f92f4872b269c85e137da3f0b714
Audrey Dutcher <audrey@rhelmot.io>
2022-04-03 20:50:17 -0700

    feature-gate vsync

--- winit/src/window.rs
+++ winit/src/window.rs
@@ -9,6 +9,11 @@ use femtovg::{renderer::OpenGl, Canvas, Color};
 use crate::cursor::translate_cursor;
 use vizia_core::{Context, Event, View, WindowDescription, WindowEvent};
 
+#[cfg(feature = "vsync")]
+const VSYNC: bool = true;
+#[cfg(not(feature = "vsync"))]
+const VSYNC: bool = false;
+
 pub struct Window {
     pub id: WindowId,
     pub canvas: Canvas<OpenGl>,
@@ -93,7 +98,7 @@ impl Window {
         #[cfg(not(target_arch = "wasm32"))]
         let handle = {
             let handle = ContextBuilder::new()
-                .with_vsync(true)
+                .with_vsync(VSYNC)
                 // .with_srgb(true)
                 .build_windowed(window_builder, &events_loop)
                 .expect("Window context creation failed!");
