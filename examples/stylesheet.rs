6988e26726b5b8cab19d4fb3222062cab488cd50
Audrey Dutcher <audrey@rhelmot.io>
2022-04-06 17:16:34 -0700

    Disable stylesheet example on wasm

--- examples/stylesheet.rs
+++ examples/stylesheet.rs
@@ -1,5 +1,8 @@
 use vizia::*;
 
+#[cfg(target_arch = "wasm32")]
+compile_error!("This example is not supported on wasm - uses filesystem");
+
 fn main() {
     Application::new(WindowDescription::new().with_title("External CSS"), |cx| {
         cx.add_stylesheet("examples/resources/test.css").expect("Failed to find file");
