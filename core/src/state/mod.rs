eedbca3dc34893ff661588f37751abbb363fe1f4
George Atkinson <geomyles@yahoo.co.uk>
2022-04-22 23:28:51 +0100

    Fix some docs examples for `state/mod.rs` and `state/model.rs`

--- core/src/state/mod.rs
+++ core/src/state/mod.rs
@@ -5,10 +5,12 @@
 //!
 //! # Example
 //! Fist we declare the data for our application. The [Lens] trait has been derived for the data, which allows us to bind to fields of the struct:
-//! ```compile_fail
-//! #[derive(Default, Lens)]
+//! ```
+//! # use vizia_core::*;
+//! # use vizia_derive::*;
+//! #[derive(Lens)]
 //! struct AppData {
-//!     some_data: bool,
+//!     count: i32,
 //! }
 //!
 //! ```
@@ -16,44 +18,74 @@
 //! to the app data to mutate it and updated values are sent to observers, such as a [`Binding`] view.
 //! ```
 //! enum AppEvent {
-//!     SetTrue,
-//!     SetFalse,
+//!     Increment,
+//!     Decrement,
 //! }
 //! ```
 //! Next we implement the [`Model`] trait on our app data, which allows us to modify the data in response to an `Event`:
-//! ```compile_fail
+//! ```
+//! # use vizia_core::*;
+//! # use vizia_derive::*;
+//! # #[derive(Lens)]
+//! # struct AppData {
+//! #     count: i32,
+//! # }
+//! # enum AppEvent {
+//! #     Increment,
+//! #     Decrement,
+//! # }
 //! impl Model for AppData {
-//!     fn on_event(&mut self, state: &mut State, entity: Entity, event: &mut Event) {
-//!         if let Some(app_event) = event.message.downcast() {
+//!     fn event(&mut self, cx: &mut Context, event: &mut Event) {
+//!         if let Some(app_event) = event.message.downcast(){
 //!             match app_event {
-//!                 AppEvent::SetTrue => {
-//!                     self.some_data = true;
+//!                 AppEvent::Increment => {
+//!                     self.count += 1;
 //!                 }
 //!
-//!                 AppEvent::SetFalse => {
-//!                     self.some_data = false;
+//!                 AppEvent::Decrement => {
+//!                     self.count -= 1;
 //!                 }
-//!             }   
+//!             }
 //!         }
 //!     }
 //! }
 //! ```
-//! This trait also allows data to be built into the [Tree](crate::Tree):
-//! ```compile_fail
+//! This trait also allows data to be built into the application [Tree](crate::Tree):
+//! ```no_run
+//! # use vizia_core::*;
+//! # use vizia_derive::*;
+//! # use vizia_winit::application::Application;
+//! # #[derive(Lens)]
+//! # struct AppData {
+//! #     count: i32,
+//! # }
+//! # impl Model for AppData {}
 //! fn main() {
 //!     Application::new(WindowDescription::new(), |cx|{
-//!         AppData::default().build(cx);
+//!         AppData {
+//!             count: 0,
+//!         }.build(cx);
 //!     }).run();  
 //! }
 //! ```
 //! A [`Binding`] view is one way in which data can be used by widgets. A [`Lens`] is used to determine what data the binding should react to:
-//! ```compile_fail
+//! ```no_run
+//! # use vizia_core::*;
+//! # use vizia_derive::*;
+//! # use vizia_winit::application::Application;
+//! # #[derive(Lens)]
+//! # struct AppData {
+//! #     count: i32,
+//! # }
+//! # impl Model for AppData {}
 //! fn main() {
 //!     Application::new(WindowDescription::new(), |cx|{
-//!         AppData::default().build(cx);
+//!         AppData {
+//!             count: 0,
+//!         }.build(cx);
 //!
-//!         Binding::new(cx, AppData::some_data, |cx, some_data|{
-//!             Label::new(cx, &some_data.get(cx).to_string());
+//!         Binding::new(cx, AppData::count, |cx, count|{
+//!             Label::new(cx, &count.get(cx).to_string());
 //!         });
 //!     }).run();
 //! }
@@ -63,18 +95,36 @@
 //! method, which takes the [Context](crate::Context) as an argument.
 //!
 //! Now when the data is modified by another widget, the label will update, for example:
-//! ```compile_fail
+//! ```no_run
+//! # use vizia_core::*;
+//! # use vizia_derive::*;
+//! # use vizia_winit::application::Application;
+//! # #[derive(Lens)]
+//! # struct AppData {
+//! #     count: i32,
+//! # }
+//! # impl Model for AppData {}
+//! # enum AppEvent {
+//! #     Increment,
+//! #     Decrement,
+//! # }
 //! fn main() {
 //!     Application::new(WindowDescription::new(), |cx|{
-//!         AppData::default().build(cx);
+//!         AppData {
+//!             count: 0,
+//!         }.build(cx);
 //!
-//!         Binding::new(cx, AppData::some_data, |cx, some_data|{
-//!             Label::new(cx, &some_data.get(cx).to_string());
+//!         Binding::new(cx, AppData::count, |cx, count|{
+//!             Label::new(cx, &count.get(cx).to_string());
 //!         });
 //!
-//!         Checkbox::new(cx, false)
-//!             .on_checked(cx, |cx| cx.emit(AppEvent::SetTrue))
-//!             .on_unchecked(cx, |cx| cx.emit(AppEvent::SetFalse));
+//!         Button::new(cx, |cx| cx.emit(AppEvent::Increment), |cx|{
+//!             Label::new(cx, "Increment")
+//!         });
+//!
+//!         Button::new(cx, |cx| cx.emit(AppEvent::Increment), |cx|{
+//!             Label::new(cx, "Decrement")
+//!         });
 //!     }).run();
 //! }
 //! ```
