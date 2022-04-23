d5daf92906c7537680f824f9b0d6298ea7593508
KDecay <KDecayMusic@protonmail.com>
2022-04-23 19:32:12 +0200

    Add EventMeta to pub use

--- core/src/events/mod.rs
+++ core/src/events/mod.rs
@@ -39,7 +39,7 @@ mod event_manager;
 pub use event_manager::EventManager;
 
 mod event;
-pub use event::{Event, Message, Propagation};
+pub use event::{Event, EventMeta, Message, Propagation};
 
 mod event_handler;
 pub use event_handler::ViewHandler;
