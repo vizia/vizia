vizia/vizia

commit 8ccf479dd259eb43ce9cb34d247046d888746a3d

Remove `downcast` boilerplate (#141)
* Remove downcast boilerplate

* Make message private

* Add EventMeta to pub use

core/src/views/scrollview.rs
@@ -63,23 +63,21 @@ pub enum ScrollUpdate {
 
 impl Model for ScrollData {
     fn event(&mut self, _cx: &mut Context, event: &mut Event) {
-        if let Some(msg) = event.message.downcast() {
-            match msg {
-                ScrollUpdate::ScrollX(f) => self.scroll_x = (self.scroll_x + *f).clamp(0.0, 1.0),
-                ScrollUpdate::ScrollY(f) => self.scroll_y = (self.scroll_y + *f).clamp(0.0, 1.0),
-                ScrollUpdate::SetX(f) => self.scroll_x = *f,
-                ScrollUpdate::SetY(f) => self.scroll_y = *f,
-                ScrollUpdate::ChildGeo(x, y) => {
-                    self.child_x = *x;
-                    self.child_y = *y;
-                }
-                ScrollUpdate::ParentGeo(x, y) => {
-                    self.parent_x = *x;
-                    self.parent_y = *y;
-                }
+        event.map(|scroll_update, _| match scroll_update {
+            ScrollUpdate::ScrollX(f) => self.scroll_x = (self.scroll_x + *f).clamp(0.0, 1.0),
+            ScrollUpdate::ScrollY(f) => self.scroll_y = (self.scroll_y + *f).clamp(0.0, 1.0),
+            ScrollUpdate::SetX(f) => self.scroll_x = *f,
+            ScrollUpdate::SetY(f) => self.scroll_y = *f,
+            ScrollUpdate::ChildGeo(x, y) => {
+                self.child_x = *x;
+                self.child_y = *y;
             }
-            event.consume();
-        }
+            ScrollUpdate::ParentGeo(x, y) => {
+                self.parent_x = *x;
+                self.parent_y = *y;
+            }
+        });
+        event.consume();
     }
 }
 
@@ -190,40 +188,38 @@ impl<L: Lens<Target = ScrollData>> View for ScrollView<L> {
     }
 
     fn event(&mut self, cx: &mut Context, event: &mut crate::Event) {
-        if let Some(window_event) = event.message.downcast() {
-            match window_event {
-                WindowEvent::GeometryChanged(geo) => {
-                    if geo.contains(GeometryChanged::HEIGHT_CHANGED)
-                        || geo.contains(GeometryChanged::WIDTH_CHANGED)
-                    {
-                        cx.emit(ScrollUpdate::ParentGeo(
-                            cx.cache.get_width(cx.current),
-                            cx.cache.get_height(cx.current),
-                        ));
-                    }
+        event.map(|window_event, _| match window_event {
+            WindowEvent::GeometryChanged(geo) => {
+                if geo.contains(GeometryChanged::HEIGHT_CHANGED)
+                    || geo.contains(GeometryChanged::WIDTH_CHANGED)
+                {
+                    cx.emit(ScrollUpdate::ParentGeo(
+                        cx.cache.get_width(cx.current),
+                        cx.cache.get_height(cx.current),
+                    ));
                 }
-
-                WindowEvent::MouseScroll(x, y) => {
-                    let (x, y) =
-                        if cx.modifiers.contains(Modifiers::SHIFT) { (-*y, *x) } else { (*x, -*y) };
-
-                    // what percentage of the negative space does this cross?
-                    let data = self.data.get(cx);
-                    if x != 0.0 {
-                        let negative_space = data.child_x - data.parent_x;
-                        let logical_delta = x * SCROLL_SENSITIVITY / negative_space;
-                        cx.emit(ScrollUpdate::ScrollX(logical_delta));
-                    }
-                    let data = cx.data::<ScrollData>().unwrap();
-                    if y != 0.0 {
-                        let negative_space = data.child_y - data.parent_y;
-                        let logical_delta = y * SCROLL_SENSITIVITY / negative_space;
-                        cx.emit(ScrollUpdate::ScrollY(logical_delta));
-                    }
-                }
-
-                _ => {}
             }
-        }
+
+            WindowEvent::MouseScroll(x, y) => {
+                let (x, y) =
+                    if cx.modifiers.contains(Modifiers::SHIFT) { (-*y, *x) } else { (*x, -*y) };
+
+                // what percentage of the negative space does this cross?
+                let data = self.data.get(cx);
+                if x != 0.0 {
+                    let negative_space = data.child_x - data.parent_x;
+                    let logical_delta = x * SCROLL_SENSITIVITY / negative_space;
+                    cx.emit(ScrollUpdate::ScrollX(logical_delta));
+                }
+                let data = cx.data::<ScrollData>().unwrap();
+                if y != 0.0 {
+                    let negative_space = data.child_y - data.parent_y;
+                    let logical_delta = y * SCROLL_SENSITIVITY / negative_space;
+                    cx.emit(ScrollUpdate::ScrollY(logical_delta));
+                }
+            }
+
+            _ => {}
+        });
     }
 }

core/src/events/event.rs
@@ -1,9 +1,7 @@
 use crate::Entity;
+use std::{any::Any, fmt::Debug};
 
-use std::any::Any;
-use std::fmt::Debug;
-
-/// Determines how the event propagates through the tree
+/// Determines how the event propagates through the tree.
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Propagation {
     // /// Events propagate down the tree to the target entity, e.g. from grand-parent to parent to child (target)
@@ -20,40 +18,30 @@ pub enum Propagation {
 
 /// A message can be any static type.
 pub trait Message: Any + Send {
-    /// An &Any can be cast to a reference to a concrete type.
-    fn as_any_mut(&mut self) -> &mut dyn Any;
+    /// A `&dyn Any` can be cast to a reference to a concrete type.
+    fn as_any(&self) -> &dyn Any;
 }
 
 impl dyn Message {
-    /// Casts a message to the specified type if the message is of that type
-    pub fn downcast<T: Message>(&mut self) -> Option<&mut T> {
-        self.as_any_mut().downcast_mut()
+    /// Casts a message to the specified type if the message is of that type.
+    pub fn downcast<T: Message>(&self) -> Option<&T> {
+        self.as_any().downcast_ref()
     }
 }
 
-// Implements message for any static type that implements Clone
+// Implements message for any static type that implements Send
 impl<S: 'static + Send> Message for S {
-    fn as_any_mut(&mut self) -> &mut dyn Any {
+    fn as_any(&self) -> &dyn Any {
         self
     }
 }
 
 /// An event is a wrapper around a message and provides metadata on how the event should be propagated through the tree
 pub struct Event {
-    // The entity that produced the event. Entity::null() for OS events or unspecified.
-    pub origin: Entity,
-    // The entity the event should be sent to. Entity::null() to send to all entities.
-    pub target: Entity,
-    // How the event propagates through the tree.
-    pub propagation: Propagation,
-    // Whether the event can be consumed
-    pub consumable: bool,
-    // Determines whether the event should continue to be propagated
-    pub(crate) consumed: bool,
-    // Specifies an order index which is used to sort the event queue
-    pub order: i32,
-    // The event message
-    pub message: Box<dyn Message>,
+    /// The meta data of the event.
+    pub meta: EventMeta,
+    /// The message of the event.
+    message: Box<dyn Message>,
 }
 
 impl Debug for Event {
@@ -77,52 +65,99 @@ impl Event {
     where
         M: Message,
     {
-        Event {
+        Event { meta: Default::default(), message: Box::new(message) }
+    }
+
+    /// Sets the target of the event.
+    pub fn target(mut self, entity: Entity) -> Self {
+        self.meta.target = entity;
+        self
+    }
+
+    /// Sets the origin of the event.
+    pub fn origin(mut self, entity: Entity) -> Self {
+        self.meta.origin = entity;
+        self
+    }
+
+    /// Sets the propagation of the event.
+    pub fn propagate(mut self, propagation: Propagation) -> Self {
+        self.meta.propagation = propagation;
+        self
+    }
+
+    /// Sets the propagation to directly target the `entity`.
+    pub fn direct(mut self, entity: Entity) -> Self {
+        self.meta.propagation = Propagation::Direct;
+        self.meta.target = entity;
+        self
+    }
+
+    /// Consumes the event to prevent it from continuing on its propagation path.
+    pub fn consume(&mut self) {
+        self.meta.consume();
+    }
+
+    /// Tries to downcast the event message to the specified type. If the downcast was successful,
+    /// the downcasted message and the event meta data get passed into `f`.
+    pub fn map<M, F>(&mut self, f: F)
+    where
+        M: Message,
+        F: FnOnce(&M, &mut EventMeta),
+    {
+        if let Some(message) = self.message.downcast() {
+            (f)(message, &mut self.meta);
+        }
+    }
+}
+
+/// The meta data of an [`Event`].
+pub struct EventMeta {
+    /// The entity that produced the event. Entity::null() for OS events or unspecified.
+    pub origin: Entity,
+    /// The entity the event should be sent to. Entity::null() to send to all entities.
+    pub target: Entity,
+    /// How the event propagates through the tree.
+    pub propagation: Propagation,
+    /// Whether the event can be consumed
+    pub consumable: bool,
+    /// Determines whether the event should continue to be propagated
+    pub(crate) consumed: bool,
+    /// Specifies an order index which is used to sort the event queue
+    pub order: i32,
+}
+
+impl EventMeta {
+    /// Creates a new event meta.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use vizia_core::*;
+    /// #
+    /// EventMeta::new();
+    /// ```
+    pub fn new() -> Self {
+        Self::default()
+    }
+}
+
+impl EventMeta {
+    /// Consumes the event to prevent it from continuing on its propagation path.
+    pub fn consume(&mut self) {
+        self.consumed = true;
+    }
+}
+
+impl Default for EventMeta {
+    fn default() -> Self {
+        Self {
             origin: Entity::null(),
             target: Entity::root(),
             propagation: Propagation::Up,
             consumable: true,
             consumed: false,
             order: 0,
-            message: Box::new(message),
         }
-    }
-
-    /// Sets the target of the event
-    pub fn target(mut self, entity: Entity) -> Self {
-        self.target = entity;
-        self
-    }
-
-    /// Sets the origin of the event
-    pub fn origin(mut self, entity: Entity) -> Self {
-        self.origin = entity;
-        self
-    }
-
-    /// Sets the propagation of the event
-    pub fn propagate(mut self, propagation: Propagation) -> Self {
-        self.propagation = propagation;
-
-        self
-    }
-
-    pub fn direct(mut self, entity: Entity) -> Self {
-        self.propagation = Propagation::Direct;
-        self.target = entity;
-        self
-    }
-
-    /// Consumes the event
-    /// (prevents the event from continuing on its propagation path)
-    pub fn consume(&mut self) {
-        self.consumed = true;
-    }
-
-    pub fn try_mut<T>(&mut self) -> Option<&mut T>
-    where
-        T: Message,
-    {
-        self.message.downcast::<T>()
     }
 }

core/src/events/event_manager.rs
@@ -36,16 +36,14 @@ impl EventManager {
         // Loop over the events in the event queue
         'events: for event in self.event_queue.iter_mut() {
             // handle internal events
-            if let Some(msg) = event.message.downcast() {
-                match msg {
-                    InternalEvent::Redraw => context.style.needs_redraw = true,
-                    InternalEvent::LoadImage { path, image, policy } => {
-                        if let Some(image) = image.lock().unwrap().take() {
-                            context.load_image(path.clone(), image, *policy);
-                        }
+            event.map(|internal_event, _| match internal_event {
+                InternalEvent::Redraw => context.style.needs_redraw = true,
+                InternalEvent::LoadImage { path, image, policy } => {
+                    if let Some(image) = image.lock().unwrap().take() {
+                        context.load_image(path.clone(), image, *policy);
                     }
                 }
-            }
+            });
 
             // if event.trace {
             //     println!("Event: {:?}", event);
@@ -68,18 +66,18 @@ impl EventManager {
                     context.listeners.insert(entity, listener);
                 }
 
-                if event.consumed {
+                if event.meta.consumed {
                     continue 'events;
                 }
             }
 
             // Define the target to prevent multiple mutable borrows error
-            let target = event.target;
+            let target = event.meta.target;
 
             // Send event to target
             visit_entity(context, target, event);
 
-            if event.consumed {
+            if event.meta.consumed {
                 continue 'events;
             }
 
@@ -88,11 +86,11 @@ impl EventManager {
             // }
 
             // Propagate up from target to root (not including target)
-            if event.propagation == Propagation::Up {
+            if event.meta.propagation == Propagation::Up {
                 // Walk up the tree from parent to parent
                 for entity in target.parent_iter(&self.tree) {
                     // Skip the target entity
-                    if entity == event.target {
+                    if entity == event.meta.target {
                         continue;
                     }
 
@@ -100,16 +98,16 @@ impl EventManager {
                     visit_entity(context, entity, event);
 
                     // Skip to the next event if the current event is consumed
-                    if event.consumed {
+                    if event.meta.consumed {
                         continue 'events;
                     }
                 }
             }
 
-            if event.propagation == Propagation::Subtree {
+            if event.meta.propagation == Propagation::Subtree {
                 for entity in target.branch_iter(&self.tree) {
                     // Skip the target entity
-                    if entity == event.target {
+                    if entity == event.meta.target {
                         continue;
                     }
 
@@ -117,7 +115,7 @@ impl EventManager {
                     visit_entity(context, entity, event);
 
                     // Skip to the next event if the current event is consumed
-                    if event.consumed {
+                    if event.meta.consumed {
                         continue 'events;
                     }
                 }

core/src/events/mod.rs
@@ -23,17 +23,15 @@
 //! The event message must then be downcast to the right type:
 //! ```compile_fail
 //! fn on_event(&mut self, state: &mut State, entity: Entity, event: &mut Event) {
-//!     if let Some(my_event) = event.message.downcast() {
-//!         match my_event {
-//!             MyEvent::ReadDocs => {
-//!                 // Do something
-//!             }
-//!
-//!             MyEvent::CloseDocs => {
-//!                 // Do something else
-//!             }
+//!     event.map(|my_event, _| match my_event {
+//!         MyEvent::ReadDocs => {
+//!             // Do something
 //!         }
-//!     }
+//!
+//!         MyEvent::CloseDocs => {
+//!             // Do something else
+//!         }
+//!     });
 //! }
 //! ```
 
@@ -41,7 +39,7 @@ mod event_manager;
 pub use event_manager::EventManager;
 
 mod event;
-pub use event::{Event, Message, Propagation};
+pub use event::{Event, EventMeta, Message, Propagation};
 
 mod event_handler;
 pub use event_handler::ViewHandler;

core/src/input/keymap.rs
@@ -60,18 +60,16 @@ use std::collections::HashMap;
 ///
 /// impl View for CustomView {
 ///     fn event(&mut self, cx: &mut Context, event: &mut Event) {
-///         if let Some(window_event) = event.message.downcast() {
-///             match window_event {
-///                 WindowEvent::KeyDown(code, _) => {
-///                     if let Some(keymap_data) = cx.data::<Keymap<Action>>() {
-///                         for action in keymap_data.pressed_actions(cx, *code) {
-///                             println!("The action {:?} is being pressed!", action);
-///                         }
+///         event.map(|window_event, _| match window_event {
+///             WindowEvent::KeyDown(code, _) => {
+///                 if let Some(keymap_data) = cx.data::<Keymap<Action>>() {
+///                     for action in keymap_data.pressed_actions(cx, *code) {
+///                         println!("The action {:?} is being pressed!", action);
 ///                     }
 ///                 }
-///                 _ => {}
 ///             }
-///         }
+///             _ => {}
+///         });
 ///     }
 /// }
 /// ```
@@ -111,7 +109,7 @@ where
     ///
     /// This method is for internal use only.
     /// To insert an entry into the keymap at runtime use the [`KeymapEvent::InsertAction`] event.
-    fn insert(&mut self, action: T, chord: KeyChord) -> &mut Self {
+    fn insert(&mut self, action: T, chord: KeyChord) {
         if let Some(actions) = self.actions.get_mut(&chord) {
             if !actions.contains(&action) {
                 actions.push(action);
@@ -119,14 +117,13 @@ where
         } else {
             self.actions.insert(chord, vec![action]);
         }
-        self
     }
 
     /// Removes an entry of the keymap.
     ///
     /// This method is for internal use only.
     /// To remove an entry of the keymap at runtime use the [`KeymapEvent::RemoveAction`] event.
-    fn remove(&mut self, action: &T, chord: &KeyChord) -> &mut Self {
+    fn remove(&mut self, action: &T, chord: &KeyChord) {
         if let Some(actions) = self.actions.get_mut(chord) {
             if let Some(index) = actions.iter().position(|x| x == action) {
                 if actions.len() == 1 {
@@ -136,7 +133,6 @@ where
                 }
             }
         }
-        self
     }
 
     /// Returns an iterator over every pressed action or `None` if there are no actions for that key chord.
@@ -205,12 +201,10 @@ where
     T: 'static + PartialEq + Send + Sync + Copy + Clone,
 {
     fn event(&mut self, _: &mut Context, event: &mut Event) {
-        if let Some(keymap_event) = event.message.downcast::<KeymapEvent<T>>() {
-            match keymap_event {
-                KeymapEvent::InsertAction(action, chord) => self.insert(*action, *chord),
-                KeymapEvent::RemoveAction(action, chord) => self.remove(action, chord),
-            };
-        }
+        event.map(|keymap_event, _| match keymap_event {
+            KeymapEvent::InsertAction(action, chord) => self.insert(*action, *chord),
+            KeymapEvent::RemoveAction(action, chord) => self.remove(action, chord),
+        });
     }
 }
 

core/src/modifiers/actions.rs
@@ -42,19 +42,17 @@ impl<V: View> View for Press<V> {
     fn event(&mut self, cx: &mut Context, event: &mut Event) {
         self.view.event(cx, event);
 
-        if let Some(window_event) = event.message.downcast() {
-            match window_event {
-                WindowEvent::MouseDown(button) if *button == MouseButton::Left => {
-                    if let Some(action) = self.action.take() {
-                        (action)(cx);
+        event.map(|window_event, _| match window_event {
+            WindowEvent::MouseDown(MouseButton::Left) => {
+                if let Some(action) = self.action.take() {
+                    (action)(cx);
 
-                        self.action = Some(action);
-                    }
+                    self.action = Some(action);
                 }
-
-                _ => {}
             }
-        }
+
+            _ => {}
+        });
     }
 
     fn draw(&self, cx: &mut DrawContext, canvas: &mut crate::Canvas) {
@@ -100,23 +98,21 @@ impl<V: View> View for Release<V> {
     fn event(&mut self, cx: &mut Context, event: &mut Event) {
         self.view.event(cx, event);
 
-        if let Some(window_event) = event.message.downcast() {
-            match window_event {
-                WindowEvent::MouseUp(button) if *button == MouseButton::Left => {
-                    if event.target == cx.current {
-                        if let Some(action) = self.action.take() {
-                            (action)(cx);
+        event.map(|window_event, meta| match window_event {
+            WindowEvent::MouseUp(MouseButton::Left) => {
+                if meta.target == cx.current {
+                    if let Some(action) = self.action.take() {
+                        (action)(cx);
 
-                            self.action = Some(action);
-                        }
-
-                        cx.release();
+                        self.action = Some(action);
                     }
-                }
 
-                _ => {}
+                    cx.release();
+                }
             }
-        }
+
+            _ => {}
+        });
     }
 
     fn draw(&self, cx: &mut DrawContext, canvas: &mut crate::Canvas) {
@@ -162,21 +158,19 @@ impl<V: View> View for Hover<V> {
     fn event(&mut self, cx: &mut Context, event: &mut Event) {
         self.view.event(cx, event);
 
-        if let Some(window_event) = event.message.downcast() {
-            match window_event {
-                WindowEvent::MouseEnter => {
-                    if event.target == cx.current {
-                        if let Some(action) = self.action.take() {
-                            (action)(cx);
+        event.map(|window_event, meta| match window_event {
+            WindowEvent::MouseEnter => {
+                if meta.target == cx.current {
+                    if let Some(action) = self.action.take() {
+                        (action)(cx);
 
-                            self.action = Some(action);
-                        }
+                        self.action = Some(action);
                     }
                 }
-
-                _ => {}
             }
-        }
+
+            _ => {}
+        });
     }
 
     fn draw(&self, cx: &mut DrawContext, canvas: &mut crate::Canvas) {
@@ -222,19 +216,17 @@ impl<V: View> View for Over<V> {
     fn event(&mut self, cx: &mut Context, event: &mut Event) {
         self.view.event(cx, event);
 
-        if let Some(window_event) = event.message.downcast() {
-            match window_event {
-                WindowEvent::MouseOver => {
-                    if let Some(action) = self.action.take() {
-                        (action)(cx);
+        event.map(|window_event, _| match window_event {
+            WindowEvent::MouseOver => {
+                if let Some(action) = self.action.take() {
+                    (action)(cx);
 
-                        self.action = Some(action);
-                    }
+                    self.action = Some(action);
                 }
-
-                _ => {}
             }
-        }
+
+            _ => {}
+        });
     }
 
     fn draw(&self, cx: &mut DrawContext, canvas: &mut crate::Canvas) {
@@ -280,21 +272,19 @@ impl<V: View> View for Leave<V> {
     fn event(&mut self, cx: &mut Context, event: &mut Event) {
         self.view.event(cx, event);
 
-        if let Some(window_event) = event.message.downcast() {
-            match window_event {
-                WindowEvent::MouseLeave => {
-                    if event.target == cx.current {
-                        if let Some(action) = self.action.take() {
-                            (action)(cx);
+        event.map(|window_event, meta| match window_event {
+            WindowEvent::MouseLeave => {
+                if meta.target == cx.current {
+                    if let Some(action) = self.action.take() {
+                        (action)(cx);
 
-                            self.action = Some(action);
-                        }
+                        self.action = Some(action);
                     }
                 }
-
-                _ => {}
             }
-        }
+
+            _ => {}
+        });
     }
 
     fn draw(&self, cx: &mut DrawContext, canvas: &mut crate::Canvas) {
@@ -340,19 +330,17 @@ impl<V: View> View for Move<V> {
     fn event(&mut self, cx: &mut Context, event: &mut Event) {
         self.view.event(cx, event);
 
-        if let Some(window_event) = event.message.downcast() {
-            match window_event {
-                WindowEvent::MouseMove(x, y) => {
-                    if let Some(action) = self.action.take() {
-                        (action)(cx, *x, *y);
+        event.map(|window_event, _| match window_event {
+            WindowEvent::MouseMove(x, y) => {
+                if let Some(action) = self.action.take() {
+                    (action)(cx, *x, *y);
 
-                        self.action = Some(action);
-                    }
+                    self.action = Some(action);
                 }
-
-                _ => {}
             }
-        }
+
+            _ => {}
+        });
     }
 
     fn draw(&self, cx: &mut DrawContext, canvas: &mut crate::Canvas) {
@@ -398,19 +386,17 @@ impl<V: View> View for FocusIn<V> {
     fn event(&mut self, cx: &mut Context, event: &mut Event) {
         self.view.event(cx, event);
 
-        if let Some(window_event) = event.message.downcast() {
-            match window_event {
-                WindowEvent::FocusIn => {
-                    if let Some(action) = self.action.take() {
-                        (action)(cx);
+        event.map(|window_event, _| match window_event {
+            WindowEvent::FocusIn => {
+                if let Some(action) = self.action.take() {
+                    (action)(cx);
 
-                        self.action = Some(action);
-                    }
+                    self.action = Some(action);
                 }
-
-                _ => {}
             }
-        }
+
+            _ => {}
+        });
     }
 
     fn draw(&self, cx: &mut DrawContext, canvas: &mut crate::Canvas) {
@@ -456,19 +442,17 @@ impl<V: View> View for FocusOut<V> {
     fn event(&mut self, cx: &mut Context, event: &mut Event) {
         self.view.event(cx, event);
 
-        if let Some(window_event) = event.message.downcast() {
-            match window_event {
-                WindowEvent::FocusOut => {
-                    if let Some(action) = self.action.take() {
-                        (action)(cx);
+        event.map(|window_event, _| match window_event {
+            WindowEvent::FocusOut => {
+                if let Some(action) = self.action.take() {
+                    (action)(cx);
 
-                        self.action = Some(action);
-                    }
+                    self.action = Some(action);
                 }
-
-                _ => {}
             }
-        }
+
+            _ => {}
+        });
     }
 
     fn draw(&self, cx: &mut DrawContext, canvas: &mut crate::Canvas) {
@@ -514,21 +498,19 @@ impl<V: View> View for Geo<V> {
     fn event(&mut self, cx: &mut Context, event: &mut Event) {
         self.view.event(cx, event);
 
-        if let Some(window_event) = event.message.downcast() {
-            match window_event {
-                WindowEvent::GeometryChanged(geo) => {
-                    if event.target == cx.current {
-                        if let Some(action) = self.action.take() {
-                            (action)(cx, *geo);
+        event.map(|window_event, meta| match window_event {
+            WindowEvent::GeometryChanged(geo) => {
+                if meta.target == cx.current {
+                    if let Some(action) = self.action.take() {
+                        (action)(cx, *geo);
 
-                            self.action = Some(action);
-                        }
+                        self.action = Some(action);
                     }
                 }
-
-                _ => {}
             }
-        }
+
+            _ => {}
+        });
     }
 
     fn draw(&self, cx: &mut DrawContext, canvas: &mut crate::Canvas) {

commit 7bd2feb786f495033870297494ad675ddf8201e1

Prevent ScrollView from consuming every event
This would prevent events emitted from within the scroll view from
reaching the outside scope.

Regression from 8ccf479dd259eb43ce9cb34d247046d888746a3d.

core/src/views/scrollview.rs
@@ -63,21 +63,24 @@ pub enum ScrollUpdate {
 
 impl Model for ScrollData {
     fn event(&mut self, _cx: &mut Context, event: &mut Event) {
-        event.map(|scroll_update, _| match scroll_update {
-            ScrollUpdate::ScrollX(f) => self.scroll_x = (self.scroll_x + *f).clamp(0.0, 1.0),
-            ScrollUpdate::ScrollY(f) => self.scroll_y = (self.scroll_y + *f).clamp(0.0, 1.0),
-            ScrollUpdate::SetX(f) => self.scroll_x = *f,
-            ScrollUpdate::SetY(f) => self.scroll_y = *f,
-            ScrollUpdate::ChildGeo(x, y) => {
-                self.child_x = *x;
-                self.child_y = *y;
+        event.map(|scroll_update, meta| {
+            match scroll_update {
+                ScrollUpdate::ScrollX(f) => self.scroll_x = (self.scroll_x + *f).clamp(0.0, 1.0),
+                ScrollUpdate::ScrollY(f) => self.scroll_y = (self.scroll_y + *f).clamp(0.0, 1.0),
+                ScrollUpdate::SetX(f) => self.scroll_x = *f,
+                ScrollUpdate::SetY(f) => self.scroll_y = *f,
+                ScrollUpdate::ChildGeo(x, y) => {
+                    self.child_x = *x;
+                    self.child_y = *y;
+                }
+                ScrollUpdate::ParentGeo(x, y) => {
+                    self.parent_x = *x;
+                    self.parent_y = *y;
+                }
             }
-            ScrollUpdate::ParentGeo(x, y) => {
-                self.parent_x = *x;
-                self.parent_y = *y;
-            }
+
+            meta.consume();
         });
-        event.consume();
     }
 }
 