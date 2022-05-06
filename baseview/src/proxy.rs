e86b470a2feda8acfc09837630851ef29cf757b6
Audrey Dutcher <audrey@rhelmot.io>
2022-05-05 20:11:43 -0700

    rebase

--- baseview/src/proxy.rs
+++ baseview/src/proxy.rs
@@ -1,7 +1,8 @@
 use lazy_static::lazy_static;
 use std::collections::VecDeque;
 use std::sync::Mutex;
-use vizia_core::{Event, EventProxy};
+use vizia_core::context::EventProxy;
+use vizia_core::events::Event;
 
 lazy_static! {
     pub(crate) static ref PROXY_QUEUE: Mutex<VecDeque<Event>> = Mutex::new(VecDeque::new());
