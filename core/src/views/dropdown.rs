11ef90eeb93373fe4b01a25ca25e408a0102256c
Audrey Dutcher <audrey@rhelmot.io>
2022-05-05 22:55:53 -0700

    Add custom dropdown docstring example

--- core/src/views/dropdown.rs
+++ core/src/views/dropdown.rs
@@ -46,8 +46,84 @@ use crate::views::popup::PopupData;
 /// .width(Pixels(100.0));
 /// ```
 ///
-/// The line marked "close the popop" is not required for anything other than closing the popup -
+/// The line marked "close the popup" is not required for anything other than closing the popup -
 /// if you leave it out, the popup will simply not close until the user clicks out of the dropdown.
+///
+/// ## Custom Dropdown
+///
+/// The dropdown doesn't have to be the current state and then a set of options - it can contain any
+/// set of views in either location. Here's an example where you can use a textbox to filter a list
+/// of checkboxes which pop up when you click the textbox:
+///
+/// ```
+/// # use vizia_core::prelude::*;
+/// # let cx = &mut Context::new();
+/// #
+/// # cx.add_theme(r#"
+/// #     dropdown popup {
+/// #         background-color: white;
+/// #     }
+/// # "#);
+///
+/// #[derive(Lens, Clone, PartialEq, Eq)]
+/// struct AppData {
+///     values: [bool; 6],
+///     filter: String,
+/// }
+///
+/// # impl Data for AppData {
+/// #     fn same(&self, other: &Self) -> bool {
+/// #         self == other
+/// #     }
+/// # }
+/// #
+/// # #[derive(Debug)]
+/// # enum AppEvent {
+/// #     SetFilter(String),
+/// #     SetValue(usize, bool),
+/// # }
+/// #
+/// # impl Model for AppData {
+/// #     fn event(&mut self, _cx: &mut Context, event: &mut Event) {
+/// #         event.map(|msg, _| {
+/// #             match msg {
+/// #                 AppEvent::SetFilter(s) => self.filter = s.clone(),
+/// #                 AppEvent::SetValue(i, b) => self.values[*i] = *b,
+/// #             }
+/// #         });
+/// #     }
+/// # }
+/// #
+/// # const LABELS: [&str; 6] = ["Bees", "Butterflies", "Dragonflies", "Crickets", "Moths", "Ladybugs"];
+/// #
+/// # AppData {
+/// #     values: [true, false, true, false, true, false],
+/// #     filter: "".to_owned(),
+/// # }.build(cx);
+///
+/// Dropdown::new(cx, |cx| {
+///     Textbox::new(cx, AppData::filter).on_edit(|cx, text| {
+///         cx.emit(AppEvent::SetFilter(text));
+///     })
+///     .width(Pixels(100.0))
+///     .height(Pixels(30.0))
+/// }, |cx| {
+///     Binding::new(cx, AppData::root, |cx, lens| {
+///         let current = lens.get(cx);
+///         for i in 0..6 {
+///             if LABELS[i].to_lowercase().contains(&current.filter.to_lowercase()) {
+///                 HStack::new(cx, move |cx| {
+///                     Checkbox::new(cx, AppData::values.map(move |x| x[i]))
+///                         .on_toggle(move |cx| {
+///                             cx.emit(AppEvent::SetValue(i, !current.values[i]));
+///                         });
+///                     Label::new(cx, LABELS[i]);
+///                 });
+///             }
+///         }
+///     });
+/// }).width(Pixels(100.0));
+/// ```
 pub struct Dropdown;
 
 impl Dropdown {
