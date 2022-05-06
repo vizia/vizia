46e2fb22421b24fe170ae2b59be378aa5aefc465
Audrey Dutcher <audrey@rhelmot.io>
2022-05-05 22:55:53 -0700

    Add custom dropdown docstring example

--- core/src/modifiers/actions.rs
+++ core/src/modifiers/actions.rs
@@ -45,6 +45,11 @@ impl<V: View> View for Press<V> {
 
         event.map(|window_event, _| match window_event {
             WindowEvent::MouseDown(MouseButton::Left) => {
+                if cx.current() != cx.hovered()
+                    && !cx.hovered().is_descendant_of(cx.tree_ref(), cx.current())
+                {
+                    return;
+                }
                 if let Some(action) = &self.action {
                     (action)(cx);
                 }
