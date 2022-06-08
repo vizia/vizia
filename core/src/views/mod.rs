33beaba668550cc90c97fd9b81bf8add143e513d
George Atkinson <geomyles@yahoo.co.uk>
2022-06-08 22:23:56 +0100

    Expose TextEvent

--- core/src/views/mod.rs
+++ core/src/views/mod.rs
@@ -37,7 +37,7 @@ pub use scrollview::{ScrollData, ScrollEvent, ScrollView};
 pub use slider::Slider;
 pub use stack::{HStack, VStack, ZStack};
 pub use table::{Table, TableColumn};
-pub use textbox::Textbox;
+pub use textbox::{TextEvent, Textbox};
 
 use crate::prelude::*;
 
