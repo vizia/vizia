0c25b8757a6f2e3860390c1be8ad82e14dc0ff0e
George Atkinson <geomyles@yahoo.co.uk>
2022-07-30 02:28:21 +0100

    Style and doc improvements for radio button
    + Add example for custom checkbox icon

--- core/src/views/checkbox.rs
+++ core/src/views/checkbox.rs
@@ -4,10 +4,29 @@ use crate::prelude::*;
 /// A checkbox used to display and toggle boolean state.
 ///
 /// Clicking on the checkbox with the left mouse button triggers the `on_toggle` callback.
-/// The checkbox cannot be used without being bound to some data, because it will not accept
-/// a raw bool as an argument.
 ///
 /// # Examples
+///
+/// ## Basic checkbox
+///
+/// The checkbox must bound to some boolean data.
+///
+/// ```
+/// # use vizia_core::prelude::*;
+/// #
+/// # #[derive(Lens)]
+/// # struct AppData {
+/// #     value: bool,
+/// # }
+/// #
+/// # impl Model for AppData {}
+/// #
+/// # let cx = &mut Context::new();
+/// #
+/// # AppData { value: false }.build(cx);
+/// #
+/// Checkbox::new(cx, AppData::value);
+/// ```
 ///
 /// ## Checkbox with an action
 ///
@@ -35,33 +54,10 @@ use crate::prelude::*;
 /// Checkbox::new(cx, AppData::value).on_toggle(|cx| cx.emit(AppEvent::ToggleValue));
 /// ```
 ///
-/// ## Checkbox without an action
-///
-/// A checkbox can be used without a callback and therefore do nothing when pressed.
-/// This is useful for prototyping and testing out the different styling options of
-/// a checkbox without having to add a callback.
-///
-/// ```
-/// # use vizia_core::prelude::*;
-/// #
-/// # #[derive(Lens)]
-/// # struct AppData {
-/// #     value: bool,
-/// # }
-/// #
-/// # impl Model for AppData {}
-/// #
-/// # let cx = &mut Context::new();
-/// #
-/// # AppData { value: false }.build(cx);
-/// #
-/// Checkbox::new(cx, AppData::value);
-/// ```
-///
 /// ## Checkbox with a label
 ///
 /// A checkbox is usually used with a label next to it describing what data the checkbox
-/// is bound to or what the checkbox does when pressed. This can for example be done by
+/// is bound to or what the checkbox does when pressed. This can be done, for example, by
 /// wrapping the checkbox in an [`HStack`](crate::prelude::HStack) and adding a [`Label`](crate::prelude::Label)
 /// to it.
 ///
@@ -83,6 +79,34 @@ use crate::prelude::*;
 ///     Checkbox::new(cx, AppData::value);
 ///     Label::new(cx, "Press me");
 /// });
+/// ```
+///
+/// ## Custom checkbox
+///
+/// The `text` modifier combined with a `map` on the lens can be used to customize the icon used by the checkbox.
+///
+/// ```
+/// # use vizia_core::prelude::*;
+/// #
+/// # #[derive(Lens)]
+/// # struct AppData {
+/// #     value: bool,
+/// # }
+/// #
+/// # impl Model for AppData {}
+/// #
+/// # enum AppEvent {
+/// #     ToggleValue,
+/// # }
+/// #
+/// # let cx = &mut Context::new();
+/// #
+/// # AppData { value: false }.build(cx);
+/// use vizia::fonts::unicode_names::CANCEL;
+///
+/// Checkbox::new(cx, AppData::value)
+///     .on_toggle(|cx| cx.emit(AppEvent::ToggleValue))
+///     .text(AppData::value.map(|flag| if *flag {CANCEL} else {""}));
 /// ```
 pub struct Checkbox {
     on_toggle: Option<Box<dyn Fn(&mut Context)>>,
@@ -173,18 +197,6 @@ impl View for Checkbox {
                 }
             }
 
-            // TODO: Should this be done manually like this or should disabled controls always revert to default cursor icon?
-            // TODO: This doesn't work in it's current form. Commented out in favour of setting mouse cursor in hover system.
-            // WindowEvent::MouseEnter => {
-            //     if meta.target == cx.current() && cx.is_disabled() {
-            //         println!("Enter: {}", cx.current());
-            //         // After context and modifiers change this will become:
-            //         // cx.cursor(CursorIcon::Default);
-            //         let current = cx.current();
-            //         cx.style().cursor.insert(current, CursorIcon::Default);
-            //         cx.need_redraw();
-            //     }
-            // }
             _ => {}
         });
     }
