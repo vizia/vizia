6513dcbd767c797a1bc101858e3d77a163ff3234
Lunae Somnia <lunaesomnia@protonmail.com>
2022-08-29 22:11:31 +0200

    calendar & spinbox small changes

--- crates/vizia_core/src/views/datepicker.rs
+++ crates/vizia_core/src/views/datepicker.rs
@@ -1,72 +1,263 @@
-use chrono::{Date, Datelike, NaiveDate, TimeZone, Utc};
+use chrono::{Datelike, NaiveDate, Utc, Weekday};
 
 use crate::prelude::*;
 
-impl<T: TimeZone + 'static> Data for Date<T> {
-    fn same(&self, other: &Self) -> bool {
-        self.eq(other)
-    }
+#[derive(Lens, Data, Clone)]
+pub struct Date {
+    day: u32,
+    month: u32,
+    year: i32,
 }
 
-pub struct Datepicker<L: Lens<Target = Date<T>>, T: TimeZone + 'static> {
-    date: L,
+#[derive(Lens)]
+pub struct Datepicker {
+    view_date: Date,
+
+    selected_date: Date,
+
+    month_str: String,
 }
+
+const MONTHS: [&str; 12] = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
+
+const DAYS_HEADER: [&str; 7] = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"];
 
 pub enum DatepickerEvent {
-    N,
+    IncrementMonth,
+    DecrementMonth,
+
+    IncrementYear,
+    DecrementYear,
+
+    SelectDate(Date),
 }
 
-impl<L: Lens<Target = Date<T>>, T: TimeZone + 'static> Datepicker<L, T> {
+impl Datepicker {
     fn is_leap_year(year: i32) -> bool {
         NaiveDate::from_ymd_opt(year, 2, 29).is_some()
     }
 
-    fn last_day_of_month(year: i32, month: u32) -> i64 {
+    fn first_day_of_month(year: i32, month: u32) -> Weekday {
+        NaiveDate::from_ymd(year, month, 1).weekday()
+    }
+
+    fn last_day_of_month(year: i32, month: u32) -> u32 {
         if month == 12 {
             NaiveDate::from_ymd(year + 1, 1, 1)
         } else {
             NaiveDate::from_ymd(year, month + 1, 1)
         }
         .signed_duration_since(NaiveDate::from_ymd(year, month, 1))
-        .num_days()
+        .num_days() as u32
     }
 
-    pub fn new(cx: &mut Context, date: L) -> Handle<Self>
+    fn render_disabled_day(cx: &mut Context, day: u32) {
+        Element::new(cx).text(&day.to_string()).class("datepicker-calendar-day-disabled");
+    }
+
+    fn render_day(cx: &mut Context, view_date: &Date, selected_date: &Date, current_day: u32) {
+        let view_date = view_date.clone();
+        Element::new(cx)
+            .text(&current_day.to_string())
+            .class("datepicker-calendar-day")
+            .checked(
+                selected_date.day == current_day
+                    && selected_date.month == view_date.month
+                    && selected_date.year == view_date.year,
+            )
+            .cursor(CursorIcon::Hand)
+            .on_press(move |ex| {
+                ex.emit(DatepickerEvent::SelectDate(Date {
+                    day: current_day,
+                    month: view_date.month,
+                    year: view_date.year,
+                }))
+            });
+    }
+
+    fn previous_month_days(year: i32, month: u32) -> u32 {
+        if month == 1 {
+            Self::last_day_of_month(year - 1, 12)
+        } else {
+            Self::last_day_of_month(year, month - 1)
+        }
+    }
+
+    pub fn new(cx: &mut Context) -> Handle<Self>
 where {
         let today = Utc::today();
-        let weekday = today.weekday();
         let day = today.day();
         let month = today.month();
         let year = today.year();
-        let days_this_month = Self::last_day_of_month(year, month);
-        println!("{}: {} {} {} {} -- {}", today, weekday, day, month, year, days_this_month);
 
-        Self { date: date.clone() }
-            .build(cx, |cx| {
-                Binding::new(cx, date, |cx, lens| {
-                    let date = lens.get(cx);
-                    Label::new(cx, &format!("{}", date.day()));
-                    Label::new(cx, &format!("{}", date.month()));
-                    Label::new(cx, &format!("{}", date.year()));
-                    Label::new(
-                        cx,
-                        &format!("{}", Self::last_day_of_month(date.year(), date.month())),
-                    );
+        Self {
+            month_str: MONTHS[month as usize - 1].to_string(),
+            view_date: Date { day, month, year },
+            selected_date: Date { day, month, year },
+        }
+        .build(cx, |cx| {
+            HStack::new(cx, |cx| {
+                Spinbox::new(cx, Datepicker::month_str, SpinboxKind::Horizontal)
+                    .left(Units::Stretch(1.0))
+                    .on_increment(|ex| ex.emit(DatepickerEvent::IncrementMonth))
+                    .on_decrement(|ex| ex.emit(DatepickerEvent::DecrementMonth));
+                Spinbox::new(cx, Datepicker::view_date.then(Date::year), SpinboxKind::Horizontal)
+                    .right(Units::Stretch(1.0))
+                    .on_increment(|ex| ex.emit(DatepickerEvent::IncrementYear))
+                    .on_decrement(|ex| ex.emit(DatepickerEvent::DecrementYear));
+            })
+            .class("datepicker-header");
+
+            Element::new(cx).class("datepicker-divisor");
+
+            VStack::new(cx, |cx| {
+                HStack::new(cx, |cx| {
+                    for h in DAYS_HEADER {
+                        Element::new(cx).text(h).class("datepicker-calendar-header");
+                    }
+                })
+                .class("datepicker-calendar-headers");
+
+                Binding::new(cx, Datepicker::view_date, |cx, view_date| {
+                    Binding::new(cx, Datepicker::selected_date, move |cx, selected_date| {
+                        let view_date = view_date.get(cx);
+                        let selected_date = selected_date.get(cx);
+
+                        let days_this_month =
+                            Self::last_day_of_month(view_date.year, view_date.month);
+
+                        let mut current_rendered_day = 0;
+
+                        let first_day_of_month =
+                            Self::first_day_of_month(view_date.year, view_date.month);
+
+                        // First week of month
+                        HStack::new(cx, |cx| {
+                            let previous_month_days =
+                                Self::previous_month_days(view_date.year, view_date.month);
+                            let filling_days = first_day_of_month.num_days_from_monday();
+                            for i in 0..filling_days {
+                                Self::render_disabled_day(
+                                    cx,
+                                    previous_month_days - (filling_days - i - 1),
+                                );
+                            }
+
+                            for i in 0..7 - filling_days {
+                                current_rendered_day += 1;
+                                Self::render_day(
+                                    cx,
+                                    &view_date,
+                                    &selected_date,
+                                    current_rendered_day,
+                                );
+                            }
+                        })
+                        .class("datepicker-calendar-row");
+
+                        // Filling weeks
+                        while current_rendered_day < days_this_month - 7 {
+                            HStack::new(cx, |cx| {
+                                for i in 0..7 {
+                                    current_rendered_day += 1;
+                                    Self::render_day(
+                                        cx,
+                                        &view_date,
+                                        &selected_date,
+                                        current_rendered_day,
+                                    );
+                                }
+                            })
+                            .class("datepicker-calendar-row");
+                        }
+
+                        // Last week of month
+                        HStack::new(cx, |cx| {
+                            let mut new_days = 1;
+                            for i in 0..7 {
+                                current_rendered_day += 1;
+                                if current_rendered_day <= days_this_month {
+                                    Self::render_day(
+                                        cx,
+                                        &view_date,
+                                        &selected_date,
+                                        current_rendered_day,
+                                    );
+                                } else {
+                                    Self::render_disabled_day(cx, new_days);
+                                    new_days += 1;
+                                }
+                            }
+                        })
+                        .class("datepicker-calendar-row");
+                    });
                 });
             })
-            .background_color(Color::rgb(24, 24, 24))
-            .keyboard_navigatable(true)
+            .class("datepicker-calendar");
+
+            Element::new(cx).class("datepicker-divisor");
+        })
+        .layout_type(LayoutType::Column)
+        .keyboard_navigatable(true)
     }
 }
 
-impl<L: Lens<Target = Date<T>>, T: TimeZone + 'static> View for Datepicker<L, T> {
+impl View for Datepicker {
     fn element(&self) -> Option<&'static str> {
         Some("datepicker")
     }
 
     fn event(&mut self, cx: &mut EventContext, event: &mut Event) {
         event.map(|e, _| match e {
-            DatepickerEvent::N => {}
+            DatepickerEvent::IncrementMonth => {
+                if self.view_date.month == 12 {
+                    self.view_date.month = 1;
+                    self.view_date.year += 1;
+                } else {
+                    self.view_date.month += 1;
+                }
+
+                println!("{}", self.view_date.month);
+
+                self.month_str = MONTHS[self.view_date.month as usize - 1].to_string();
+            }
+
+            DatepickerEvent::DecrementMonth => {
+                if self.view_date.month == 1 {
+                    self.view_date.month = 12;
+                    self.view_date.year -= 1;
+                } else {
+                    self.view_date.month -= 1;
+                }
+                println!("{}", self.view_date.month);
+
+                self.month_str = MONTHS[self.view_date.month as usize - 1].to_string();
+            }
+
+            DatepickerEvent::IncrementYear => {
+                self.view_date.year += 1;
+            }
+
+            DatepickerEvent::DecrementYear => {
+                self.view_date.year -= 1;
+            }
+
+            DatepickerEvent::SelectDate(date) => {
+                self.selected_date = date.clone();
+            }
         })
     }
 }
--- examples/views/datepicker.rs
+++ examples/views/datepicker.rs
@@ -1,23 +1,36 @@
 use chrono::{Date, Utc};
 use vizia::prelude::*;
 
-#[derive(Clone, Data, Lens)]
+#[derive(Clone, Lens)]
 struct AppState {
     date: Date<Utc>,
 }
+
+#[allow(dead_code)]
+const DARK_THEME: &str = "crates/vizia_core/resources/themes/dark_theme.css";
+#[allow(dead_code)]
+const LIGHT_THEME: &str = "crates/vizia_core/resources/themes/light_theme.css";
 
 fn main() {
     Application::new(|cx| {
         AppState { date: Utc::today() }.build(cx);
 
-        HStack::new(cx, |cx| {
-            // Basic Datepicker
-            Datepicker::new(cx, AppState::date);
+        cx.add_stylesheet(DARK_THEME).expect("Failed to find stylesheet");
+
+        VStack::new(cx, |cx| {
+            HStack::new(cx, |cx| {
+                Datepicker::new(cx);
+            })
+            .size(Auto)
+            .row_between(Pixels(10.0))
+            .space(Stretch(1.0));
         })
-        .size(Auto)
-        .space(Stretch(1.0))
-        .col_between(Pixels(10.0));
+        .class("main")
+        .width(Units::Stretch(1.0))
+        .height(Units::Stretch(1.0));
     })
+    //.ignore_default_theme()
+    .ignore_default_theme()
     .title("Datepicker")
     .run();
 }
