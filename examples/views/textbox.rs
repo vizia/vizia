744f0417256f14bcf8c103894c9e149a296c77fd
Lunae Somnia <lunaesomnia@protonmail.com>
2022-08-30 15:35:11 +0200

    Textbox

--- examples/views/textbox.rs
+++ examples/views/textbox.rs
@@ -20,17 +20,47 @@ impl Model for AppData {
     }
 }
 
+#[allow(dead_code)]
+const DARK_THEME: &str = "crates/vizia_core/resources/themes/dark_theme.css";
+#[allow(dead_code)]
+const LIGHT_THEME: &str = "crates/vizia_core/resources/themes/light_theme.css";
+
 fn main() {
     Application::new(|cx| {
         AppData { text: "This text is editable!".to_string() }.build(cx);
 
-        Textbox::new(cx, AppData::text)
-            .on_edit(|cx, text| cx.emit(AppEvent::SetText(text)))
-            .width(Pixels(200.0))
-            .on_build(|cx| {
-                cx.emit(TextEvent::StartEdit);
-            });
+        cx.add_stylesheet(LIGHT_THEME).expect("Failed to find stylesheet");
+
+        VStack::new(cx, |cx| {
+            VStack::new(cx, |cx| {
+                Textbox::new(cx, AppData::text)
+                    .on_edit(|cx, text| cx.emit(AppEvent::SetText(text)))
+                    .width(Pixels(200.0))
+                    .on_build(|cx| {
+                        cx.emit(TextEvent::StartEdit);
+                    });
+
+                Textbox::new(cx, AppData::text)
+                    .on_edit(|cx, text| cx.emit(AppEvent::SetText(text)))
+                    .width(Pixels(200.0))
+                    .on_build(|cx| {
+                        cx.emit(TextEvent::StartEdit);
+                    })
+                    .disabled(true);
+
+                Textbox::new(cx, AppData::text)
+                    .on_edit(|cx, text| cx.emit(AppEvent::SetText(text)))
+                    .width(Pixels(200.0))
+                    .on_build(|cx| {
+                        cx.emit(TextEvent::StartEdit);
+                    })
+                    .class("error");
+            })
+            .class("container");
+        })
+        .class("main");
     })
+    .ignore_default_theme()
     .title("Textbox")
     .run();
 }
