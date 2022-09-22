65c2e32093a39ab58277452ea4dc9724f7165c63
George Atkinson <geomyles@yahoo.co.uk>
2022-09-22 20:44:41 +0100

    Export `GeneratioalId` through prelude to allow constructing `Entity::root()`

--- crates/vizia_core/src/lib.rs
+++ crates/vizia_core/src/lib.rs
@@ -54,6 +54,7 @@ pub mod prelude {
     pub use super::views::*;
     pub use super::window::WindowModifiers;
     pub use vizia_derive::{Data, Lens, Model, Setter};
+    pub use vizia_id::GenerationalId;
     pub use vizia_input::{Code, Key, KeyChord, Modifiers, MouseButton, MouseButtonState};
     pub use vizia_storage::{Tree, TreeExt};
     pub use vizia_window::{CursorIcon, WindowDescription, WindowEvent, WindowSize};
