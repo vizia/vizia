8bca499d549618664e8b190323d0579a647b3dc1
Lunae Somnia <lunaesomnia@protonmail.com>
2022-09-27 22:35:26 +0200

    restore dark theme

--- examples/views/button.rs
+++ examples/views/button.rs
@@ -8,7 +8,7 @@ const LIGHT_THEME: &str = "crates/vizia_core/resources/themes/light_theme.css";
 
 fn main() {
     Application::new(|cx| {
-        cx.add_stylesheet(LIGHT_THEME).expect("Failed to find stylesheet");
+        cx.add_stylesheet(DARK_THEME).expect("Failed to find stylesheet");
 
         VStack::new(cx, |cx| {
             VStack::new(cx, |cx| {
