12d6ba8c7e066d1c09811ae65431bbf68124dd18
Lunae Somnia <lunaesomnia@protonmail.com>
2022-09-28 08:51:29 +0200

    using root pseudoclass for background color

--- examples/views/button.rs
+++ examples/views/button.rs
@@ -11,27 +11,24 @@ fn main() {
         cx.add_stylesheet(DARK_THEME).expect("Failed to find stylesheet");
 
         VStack::new(cx, |cx| {
-            VStack::new(cx, |cx| {
-                // Basic Button
-                Button::new(cx, |cx| Label::new(cx, "Button"));
-                // Accent Button
-                Button::new(cx, |cx| Label::new(cx, "Another Button")).class("accent");
-                // Disabled Button
-                Button::new(cx, |cx| Label::new(cx, "Disabled Button")).disabled(true);
-                // Button with Icon
-                Button::new(cx, |cx| {
-                    HStack::new(cx, |cx| {
-                        Label::new(cx, CHECK).class("icon");
-                        Label::new(cx, "Button");
-                    })
-                    .size(Auto)
-                    .child_space(Stretch(1.0))
-                    .col_between(Pixels(2.0))
-                });
-            })
-            .class("container");
+            // Basic Button
+            Button::new(cx, |cx| Label::new(cx, "Button"));
+            // Accent Button
+            Button::new(cx, |cx| Label::new(cx, "Another Button")).class("accent");
+            // Disabled Button
+            Button::new(cx, |cx| Label::new(cx, "Disabled Button")).disabled(true);
+            // Button with Icon
+            Button::new(cx, |cx| {
+                HStack::new(cx, |cx| {
+                    Label::new(cx, CHECK).class("icon");
+                    Label::new(cx, "Button");
+                })
+                .size(Auto)
+                .child_space(Stretch(1.0))
+                .col_between(Pixels(2.0))
+            });
         })
-        .class("main");
+        .class("container");
     })
     .ignore_default_theme()
     .title("Button")
