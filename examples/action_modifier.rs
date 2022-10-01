75b6f282dd7216968474f9afcc70fe4d778a22bd
George Atkinson <geomyles@yahoo.co.uk>
2022-10-01 23:25:09 +0100

    Update action_modifier.rs

--- examples/action_modifier.rs
+++ examples/action_modifier.rs
@@ -4,7 +4,7 @@ use vizia::prelude::*;
 fn main() {
     Application::new(|cx| {
         Label::new(cx, "Press on me!").on_press(|_| println!("You pressed on a label!"));
-        Label::new(cx, "Release on me!").on_release(|_| println!("You released on a label!"));
+        Label::new(cx, "Release on me!").on_mouse_up(|_, _| println!("You released on a label!"));
         Label::new(cx, "Hover on me!").on_hover(|_| println!("You hovered a label!"));
         Label::new(cx, "Right click on me!").on_mouse_down(|_, button| {
             if button == MouseButton::Right {
