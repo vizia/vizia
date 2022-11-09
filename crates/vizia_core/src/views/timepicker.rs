c372e0f04c57709f3ae7164d66a8601b802eb736
George Atkinson <geomyles@yahoo.co.uk>
2022-11-09 00:05:50 +0000

    Simplify timepicker view

--- crates/vizia_core/src/views/timepicker.rs
+++ crates/vizia_core/src/views/timepicker.rs
@@ -1,5 +1,3 @@
-use std::sync::Arc;
-
 use crate::prelude::*;
 
 #[derive(PartialEq, Data, Clone, Copy, Debug)]
@@ -22,31 +20,23 @@ pub enum TimepickerEvent {
     DecrementHour,
     DecrementMinutes,
     ToggleAMOrPM,
-    SetOnChanging(Option<Arc<dyn Fn(&mut EventContext, DayTime) + Send + Sync>>),
-    SetOnOk(Option<Arc<dyn Fn(&mut EventContext) + Send + Sync>>),
 }
 
-#[derive(Lens, Clone)]
-pub struct Timepicker<L: Clone + Lens<Target = DayTime>> {
-    lens: L,
-    temp: DayTime,
-    on_changing: Option<Arc<dyn Fn(&mut EventContext, DayTime) + Send + Sync>>,
-    on_ok: Option<Arc<dyn Fn(&mut EventContext) + Send + Sync>>,
+#[derive(Lens)]
+pub struct Timepicker {
+    current: DayTime,
+    on_changing: Option<Box<dyn Fn(&mut EventContext, DayTime) + Send + Sync>>,
+    on_ok: Option<Box<dyn Fn(&mut EventContext) + Send + Sync>>,
 }
 
-impl<L> Timepicker<L>
-where
-    L: Lens<Target = DayTime> + Clone,
-{
-    pub fn new(cx: &mut Context, lens: L) -> Handle<Self> {
-        Self {
-            lens: lens.clone(),
-            on_changing: None,
-            on_ok: None,
-            temp: lens.clone().get(cx).clone(),
-        }
-        .build(cx, move |cx| {
-            Binding::new(cx, lens.clone(), |cx, lens| {
+impl Timepicker {
+    pub fn new<L>(cx: &mut Context, lens: L) -> Handle<Self>
+    where
+        L: Lens<Target = DayTime>,
+    {
+        Self { on_changing: None, on_ok: None, current: lens.clone().get(cx).clone() }.build(
+            cx,
+            move |cx| {
                 HStack::new(cx, |cx| {
                     Spinbox::new(cx, lens.clone().then(DayTime::hour), SpinboxKind::Vertical)
                         .on_increment(|ex| ex.emit(TimepickerEvent::IncrementHour))
@@ -64,15 +54,12 @@ where
                             cx,
                             |cx| cx.emit(TimepickerEvent::ToggleAMOrPM),
                             |cx| {
-                                Label::new(cx, "x").bind(
-                                    lens.then(DayTime::zone),
-                                    |handle, lens| {
-                                        let lens = lens.get(handle.cx);
-                                        match lens {
-                                            AMOrPM::AM => handle.text("AM"),
-                                            AMOrPM::PM => handle.text("PM"),
-                                        };
-                                    },
+                                Label::new(
+                                    cx,
+                                    lens.then(DayTime::zone).map(|zone| match zone {
+                                        AMOrPM::AM => "AM",
+                                        AMOrPM::PM => "PM",
+                                    }),
                                 )
                             },
                         );
@@ -88,38 +75,28 @@ where
                     .class("timepicker-button-wrapper");
                 })
                 .class("timepicker-wrapper");
-            });
-        })
+            },
+        )
     }
 }
 
-impl<'a, L> Handle<'a, Timepicker<L>>
-where
-    L: Lens<Target = DayTime>,
-{
+impl<'a> Handle<'a, Timepicker> {
     pub fn on_changing<F>(self, callback: F) -> Self
     where
         F: 'static + Fn(&mut EventContext, DayTime) + Send + Sync,
     {
-        self.cx.emit_to(self.entity(), TimepickerEvent::SetOnChanging(Some(Arc::new(callback))));
-
-        self
+        self.modify(|timepicker: &mut Timepicker| timepicker.on_changing = Some(Box::new(callback)))
     }
 
     pub fn on_ok<F>(self, callback: F) -> Self
     where
         F: 'static + Fn(&mut EventContext) + Send + Sync,
     {
-        self.cx.emit_to(self.entity(), TimepickerEvent::SetOnOk(Some(Arc::new(callback))));
-
-        self
+        self.modify(|timepicker: &mut Timepicker| timepicker.on_ok = Some(Box::new(callback)))
     }
 }
 
-impl<L> View for Timepicker<L>
-where
-    L: Lens<Target = DayTime>,
-{
+impl View for Timepicker {
     fn element(&self) -> Option<&'static str> {
         Some("timepicker")
     }
@@ -127,46 +104,46 @@ where
     fn event(&mut self, cx: &mut EventContext, event: &mut Event) {
         event.map(|e, _| match e {
             TimepickerEvent::IncrementHour => {
-                self.temp.hour += 1;
-                if self.temp.hour > 12 {
-                    self.temp.hour -= 12;
+                self.current.hour += 1;
+                if self.current.hour > 12 {
+                    self.current.hour -= 12;
                 }
 
                 if let Some(callback) = &self.on_changing {
-                    (callback)(cx, self.temp.clone())
+                    (callback)(cx, self.current)
                 }
             }
 
             TimepickerEvent::IncrementMinutes => {
-                self.temp.minutes += 5;
-                if self.temp.minutes >= 60 {
-                    self.temp.minutes -= 60;
+                self.current.minutes += 1;
+                if self.current.minutes >= 60 {
+                    self.current.minutes -= 60;
                 }
 
                 if let Some(callback) = &self.on_changing {
-                    (callback)(cx, self.temp.clone())
+                    (callback)(cx, self.current)
                 }
             }
 
             TimepickerEvent::DecrementHour => {
-                self.temp.hour -= 1;
-                if self.temp.hour == 0 {
-                    self.temp.hour += 12;
+                self.current.hour -= 1;
+                if self.current.hour == 0 {
+                    self.current.hour += 12;
                 }
 
                 if let Some(callback) = &self.on_changing {
-                    (callback)(cx, self.temp.clone())
+                    (callback)(cx, self.current)
                 }
             }
 
             TimepickerEvent::DecrementMinutes => {
-                if self.temp.minutes < 5 {
-                    self.temp.minutes += 60;
+                if self.current.minutes < 5 {
+                    self.current.minutes += 60;
                 }
-                self.temp.minutes -= 5;
+                self.current.minutes -= 5;
 
                 if let Some(callback) = &self.on_changing {
-                    (callback)(cx, self.temp.clone())
+                    (callback)(cx, self.current)
                 }
             }
 
@@ -177,22 +154,14 @@ where
             }
 
             TimepickerEvent::ToggleAMOrPM => {
-                match self.temp.zone {
-                    AMOrPM::AM => self.temp.zone = AMOrPM::PM,
-                    AMOrPM::PM => self.temp.zone = AMOrPM::AM,
+                match self.current.zone {
+                    AMOrPM::AM => self.current.zone = AMOrPM::PM,
+                    AMOrPM::PM => self.current.zone = AMOrPM::AM,
                 }
 
                 if let Some(callback) = &self.on_changing {
-                    (callback)(cx, self.temp.clone())
+                    (callback)(cx, self.current.clone())
                 }
-            }
-
-            TimepickerEvent::SetOnChanging(callback) => {
-                self.on_changing = callback.clone();
-            }
-
-            TimepickerEvent::SetOnOk(callback) => {
-                self.on_ok = callback.clone();
             }
         })
     }
