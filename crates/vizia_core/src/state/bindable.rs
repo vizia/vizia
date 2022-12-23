bc85cd56b31d104065b1f2ffc5cfae69e5be5080
George Atkinson <geomyles@yahoo.co.uk>
2022-12-23 02:47:26 +0000

    Rename `get_val2` to `get`. Move `BindableExt` code

--- crates/vizia_core/src/state/bindable.rs
+++ crates/vizia_core/src/state/bindable.rs
@@ -1,6 +1,7 @@
 use std::{
     any::TypeId,
     collections::{HashMap, HashSet},
+    marker::PhantomData,
 };
 
 use vizia_storage::TreeExt;
@@ -15,7 +16,7 @@ use super::{BasicStore, Data, Lens, Store};
 
 pub trait Bindable: 'static + Clone {
     type Output;
-    fn get_val2<C: DataContext>(&self, cx: &C) -> Self::Output;
+    fn get<C: DataContext>(&self, cx: &C) -> Self::Output;
     fn insert_store(self, cx: &mut Context, entity: Entity);
     fn name(&self) -> Option<&'static str>;
 }
@@ -26,7 +27,7 @@ where
 {
     type Output = L::Target;
 
-    fn get_val2<C: DataContext>(&self, cx: &C) -> Self::Output {
+    fn get<C: DataContext>(&self, cx: &C) -> Self::Output {
         self.view(
             cx.data().expect("Failed to get data from context. Has it been built into the tree?"),
             |t| {
@@ -131,8 +132,8 @@ where
 {
     type Output = (L1::Target, L2::Target);
 
-    fn get_val2<C: DataContext>(&self, cx: &C) -> Self::Output {
-        (self.0.get_val2(cx), self.1.get_val2(cx))
+    fn get<C: DataContext>(&self, cx: &C) -> Self::Output {
+        (self.0.get(cx), self.1.get(cx))
     }
 
     fn insert_store(self, cx: &mut Context, id: Entity) {
@@ -146,3 +147,168 @@ where
         self.0.name()
     }
 }
+
+#[derive(Clone)]
+pub struct BindMap<B, F, T> {
+    b: B,
+    map: F,
+    p: PhantomData<T>,
+}
+
+impl<B, F, T> BindMap<B, F, T> {
+    pub fn new(b: B, map: F) -> Self
+    where
+        B: Bindable + Clone,
+        F: Fn(&B::Output) -> T,
+    {
+        Self { b, map, p: PhantomData::default() }
+    }
+}
+
+impl<B: Copy, F: Copy, T: Clone> Copy for BindMap<B, F, T> {}
+
+impl<B, F, T: Clone + 'static> Bindable for BindMap<B, F, T>
+where
+    B: 'static + Bindable + Clone,
+    F: 'static + Clone + Fn(&B::Output) -> T,
+{
+    type Output = T;
+    fn get<C: DataContext>(&self, cx: &C) -> Self::Output {
+        (self.map)(&self.b.get(cx))
+    }
+
+    fn insert_store(self, cx: &mut Context, entity: Entity) {
+        self.b.insert_store(cx, entity);
+    }
+
+    fn name(&self) -> Option<&'static str> {
+        self.b.name()
+    }
+}
+
+#[derive(Clone)]
+pub struct BindIndex<B, T> {
+    b: B,
+    index: usize,
+    p: PhantomData<T>,
+}
+
+impl<B, T> BindIndex<B, T> {
+    pub fn new(b: B, index: usize) -> Self {
+        Self { b, index, p: PhantomData::default() }
+    }
+}
+
+impl<B: Copy, T: Clone> Copy for BindIndex<B, T> {}
+
+impl<B, T: 'static> Bindable for BindIndex<B, T>
+where
+    B: Bindable,
+    <B as Bindable>::Output: std::ops::Deref<Target = [T]>,
+    T: Clone,
+{
+    type Output = T;
+    fn get<C: DataContext>(&self, cx: &C) -> Self::Output {
+        if let Some(val) = self.b.get(cx).get(self.index) {
+            val.clone()
+        } else {
+            panic!("Failed");
+        }
+    }
+
+    fn insert_store(self, cx: &mut Context, entity: Entity) {
+        self.b.insert_store(cx, entity);
+    }
+
+    fn name(&self) -> Option<&'static str> {
+        self.b.name()
+    }
+}
+
+#[derive(Clone)]
+pub struct BindThen<B, L, T> {
+    b: B,
+    l: L,
+    p: PhantomData<T>,
+}
+
+impl<B: Copy, L: Copy, T: Clone> Copy for BindThen<B, L, T> {}
+
+impl<B, L, T> BindThen<B, L, T> {
+    pub fn new(b: B, l: L) -> Self {
+        Self { b, l, p: PhantomData::default() }
+    }
+}
+
+// impl<B: Copy, T: Clone> Copy for BindIndex<B, T> {}
+
+impl<B, L, T: 'static> Bindable for BindThen<B, L, T>
+where
+    B: Bindable,
+    L: Lens<Source = B::Output, Target = T>,
+    T: Clone,
+{
+    type Output = L::Target;
+    fn get<C: DataContext>(&self, cx: &C) -> Self::Output {
+        self.l.view(&self.b.get(cx), |t| t.cloned().expect("Failed")).clone()
+    }
+
+    fn insert_store(self, cx: &mut Context, entity: Entity) {
+        self.b.insert_store(cx, entity);
+    }
+
+    fn name(&self) -> Option<&'static str> {
+        self.b.name()
+    }
+}
+// pub trait BindableExt: Clone {
+//     type Map<S, F, T>;
+//     type Output;
+//     fn map<F, T: 'static>(self, f: F) -> Self::Map<Self, F, T>
+//     where
+//         F: 'static + Clone + Fn(&Self::Output) -> T;
+// }
+
+impl<B1, L1, T1, B2, L2, T2> Bindable for (BindThen<B1, L1, T1>, BindThen<B2, L2, T2>)
+where
+    B1: Bindable,
+    B2: Bindable,
+    L1: Lens<Source = B1::Output, Target = T1>,
+    L2: Lens<Source = B2::Output, Target = T2>,
+    T1: 'static + Clone,
+    T2: 'static + Clone,
+{
+    type Output = (L1::Target, L2::Target);
+
+    fn get<C: DataContext>(&self, cx: &C) -> Self::Output {
+        (self.0.get(cx), self.1.get(cx))
+    }
+
+    fn insert_store(self, cx: &mut Context, entity: Entity) {
+        self.0.insert_store(cx, entity);
+        self.1.insert_store(cx, entity);
+    }
+
+    fn name(&self) -> Option<&'static str> {
+        todo!()
+    }
+}
+
+pub trait BindableExt: Bindable + Clone {
+    fn map<F, T: 'static>(self, f: F) -> BindMap<Self, F, T>
+    where
+        F: 'static + Clone + Fn(&Self::Output) -> T,
+    {
+        BindMap::new(self, f)
+    }
+
+    fn index<T: 'static>(self, index: usize) -> BindIndex<Self, T> {
+        BindIndex::new(self, index)
+    }
+
+    fn then<L: Lens, T: 'static>(self, lens: L) -> BindThen<Self, L, T> {
+        BindThen::new(self, lens)
+    }
+}
+
+impl<B: Bindable + Clone> BindableExt for B {}
