bf6e5ade07a41118d089ab4646919623ee434c4a
George Atkinson <geomyles@yahoo.co.uk>
2023-04-03 15:16:46 +0100

    Add theming for checkbox

--- examples/views/helpers/mod.rs
+++ examples/views/helpers/mod.rs
@@ -129,6 +129,77 @@ pub fn view_controls(cx: &mut Context) {
 pub struct ExamplePage {}
 
 impl ExamplePage {
+    pub fn vertical(cx: &mut Context, content: impl FnOnce(&mut Context)) -> Handle<Self> {
+        cx.add_stylesheet(CENTER_LAYOUT).expect("Failed to find stylesheet");
+
+        Self {}.build(cx, |cx| {
+            ControlsData { disabled: false }.build(cx);
+
+            HStack::new(cx, |cx| {
+                // PickList::new(
+                //     cx,
+                //     StaticLens::new(THEME_MODES.as_ref()),
+                //     Environment::theme_mode.map(|mode| if *mode == ThemeMode::LightMode { 0 } else { 1 }),
+                //     true,
+                // )
+                // .on_select(|cx, idx| {
+                //     if idx == 0 {
+                //         cx.emit(EnvironmentEvent::SetThemeMode(ThemeMode::LightMode));
+                //     } else {
+                //         cx.emit(EnvironmentEvent::SetThemeMode(ThemeMode::DarkMode));
+                //     }
+                // })
+                // .width(Pixels(100.0));
+
+                Button::new(
+                    cx,
+                    |ex| ex.emit(EnvironmentEvent::ToggleThemeMode),
+                    |cx| {
+                        Label::new(
+                            cx,
+                            Environment::theme_mode.map(|mode| {
+                                if *mode == ThemeMode::DarkMode {
+                                    ICON_SUN
+                                } else {
+                                    ICON_MOON
+                                }
+                            }),
+                        )
+                        .class("icon")
+                    },
+                )
+                .class("icon");
+
+                // HStack::new(cx, |cx| {
+                //     Switch::new(cx, ControlsData::disabled)
+                //         .on_toggle(|cx| cx.emit(ControlsEvent::ToggleDisabled));
+                //     Label::new(cx, "Toggle Disabled");
+                // })
+                // .child_top(Stretch(1.0))
+                // .child_bottom(Stretch(1.0))
+                // .col_between(Pixels(5.0))
+                // .top(Stretch(1.0))
+                // .bottom(Stretch(1.0))
+                // .size(Auto);
+            })
+            .height(Auto)
+            .width(Stretch(1.0))
+            .child_top(Pixels(10.0))
+            .child_bottom(Pixels(10.0))
+            .child_left(Stretch(1.0))
+            .child_right(Pixels(20.0))
+            .top(Pixels(0.0))
+            .left(Pixels(0.0))
+            .right(Pixels(0.0))
+            .col_between(Pixels(20.0));
+
+            VStack::new(cx, |cx| {
+                (content)(cx);
+            })
+            .disabled(ControlsData::disabled)
+            .class("container");
+        })
+    }
     pub fn new(cx: &mut Context, content: impl FnOnce(&mut Context)) -> Handle<Self> {
         cx.add_stylesheet(CENTER_LAYOUT).expect("Failed to find stylesheet");
 
