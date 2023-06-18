f184c35a9f43eb1c8df2926f9c20c9390a258e56
George Atkinson <geomyles@yahoo.co.uk>
2023-06-18 17:32:45 +0100

    Update properties.rs

--- examples/style/properties.rs
+++ examples/style/properties.rs
@@ -2,28 +2,28 @@ use lazy_static::lazy_static;
 use vizia::prelude::*;
 
 lazy_static! {
-    pub static ref STATIC_LIST: Vec<&'static str> = vec!["Background Image", "Background Size"];
+    pub static ref STATIC_LIST: Vec<&'static str> = vec!["Background Image", "Background Size", "Border", "Box Shadow"];
 }
 
 const BACKGROUND_IMAGE_STYLE: &str = r#"
-        .auto-size {
-            background-image: url("sample.png");
-            width: auto;
-            height: auto;
-        }
+    .auto-size {
+        background-image: url("sample.png");
+        width: auto;
+        height: auto;
+    }
 
-        .fixed-size {
-            background-image: url("sample.png");
-            width: 600px;
-            height: auto;
-        }
+    .fixed-size {
+        background-image: url("sample.png");
+        width: 600px;
+        height: auto;
+    }
 
-        .web-image {
-            background-image: url("https://download.samplelib.com/png/sample-bumblebee-400x300.png");
-            width: auto;
-            height: auto;
-        }
-    "#;
+    .web-image {
+        background-image: url("https://download.samplelib.com/png/sample-bumblebee-400x300.png");
+        width: auto;
+        height: auto;
+    }
+"#;
 
 const BACKGROUND_SIZE_STYLE: &str = r#"
     .container {
@@ -31,7 +31,7 @@ const BACKGROUND_SIZE_STYLE: &str = r#"
         child-space: 1s;
     }
 
-    .element {
+    .image_element {
         background-image: url("sample.png");
         width: 250px;
         height: 200px;
@@ -63,23 +63,194 @@ const BACKGROUND_SIZE_STYLE: &str = r#"
         background-size: cover;
     }
 
-    "#;
+"#;
+
+const BORDER_STYLE: &str = r#"
+
+    .row {
+        child-space: 1s;
+        col-between: 20px;
+    }
+
+    .border_element {
+        size: 100px;
+        background-color: rgb(200, 200, 200);
+    }
+
+    .border {
+        border: black 5px;
+    }
+
+    .border:hover {
+        border: 10px blue;
+        transition: border 0.1s;
+    }
+
+    .border_radius {
+        border-radius: 5px 10px 15px 20px;
+    }
+
+    .border_radius:hover {
+        border-radius: 10px 20px 30px 40px;
+        transition: border-radius 0.1s;
+    }
+
+    .border_shape {
+        border-radius: 30px;
+        border-corner-shape: round bevel round bevel;
+    }
+
+    .border_shape:hover {
+        border-radius: 30px;
+        border-corner-shape: bevel round bevel round;
+    }
+"#;
+
+const STYLE: &str = r#"
+
+    :root {
+        child-space: 1s;
+    }
+
+    hstack {
+        child-space: 1s;
+        col-between: 40px;
+    }
+
+    .shadow_element {
+        size: 100px;
+        background-color: rgb(200, 200, 200);
+    }
+
+    .shadow-offsetx {
+        box-shadow: 5px 0px red;
+    }
+
+    .shadow-offsetx:hover {
+        box-shadow: 15px 0px red;
+        transition: box-shadow 100ms;
+    }
+
+    .shadow-offsety {
+        box-shadow: 0px 5px red;
+    }
+
+    .shadow-offsety:hover {
+        box-shadow: 0px 15px red;
+        transition: box-shadow 100ms;
+    }
+
+    .shadow-offset {
+        box-shadow: 5px 5px red;
+    }
+
+    .shadow-offset:hover {
+        box-shadow: 15px 15px red;
+        transition: box-shadow 100ms;
+    }
+
+    .shadow-color {
+        box-shadow: 5px 5px red;
+    }
+
+    .shadow-color:hover {
+        box-shadow: 5px 5px blue;
+        transition: box-shadow 200ms;
+    }
+
+    .shadow-blur {
+        box-shadow: 5px 5px 5px red;
+    }
+
+    .shadow-blur:hover {
+        box-shadow: 5px 5px 15px red;
+        transition: box-shadow 100ms;
+    }
+    
+    .shadow {
+        box-shadow: 5px 5px blue, 10px 10px red, 15px 15px green;
+    }
+
+    .shadow:hover {
+        box-shadow: 10px 10px 16px 8px blue, 20px 20px 16px 8px red, 30px 30px 16px 8px green;
+        transition: box-shadow 200ms;
+    }
+
+    .shadow-inset-offsetx {
+        box-shadow: 5px 0px red inset;
+    }
+
+    .shadow-inset-offsetx:hover {
+        box-shadow: 15px 0px red inset;
+        transition: box-shadow 100ms;
+    }
+
+    .shadow-inset-offsety {
+        box-shadow: 0px 5px red inset;
+    }
+
+    .shadow-inset-offsety:hover {
+        box-shadow: 0px 15px red inset;
+        transition: box-shadow 100ms;
+    }
+
+    .shadow-inset-offset {
+        box-shadow: 5px 5px red inset;
+    }
+
+    .shadow-inset-offset:hover {
+        box-shadow: 15px 15px red inset;
+        transition: box-shadow 100ms;
+    }
+
+    .shadow-inset-color {
+        box-shadow: 5px 5px red inset;
+    }
+
+    .shadow-inset-color:hover {
+        box-shadow: 5px 5px blue inset;
+        transition: box-shadow 200ms;
+    }
+
+    .shadow-inset-blur {
+        box-shadow: 5px 5px 5px red inset;
+    }
+
+    .shadow-inset-blur:hover {
+        box-shadow: 5px 5px 15px red inset;
+        transition: box-shadow 100ms;
+    }
+
+    .shadow-inset {
+        box-shadow: 5px 5px blue inset, 10px 10px red inset, 15px 15px green inset;
+    }
+
+    .shadow-inset:hover {
+        box-shadow: 10px 10px 16px blue inset, 20px 20px 16px red inset, 30px 30px 16px green inset;
+        transition: box-shadow 200ms;
+    }
+"#;
 
 fn main() {
     Application::new(|cx| {
-        // Load an image into the binary
-        cx.load_image(
-            "sample.png",
-            image::load_from_memory_with_format(
-                include_bytes!("../resources/images/sample-hut-400x300.png"),
-                image::ImageFormat::Png,
-            )
-            .unwrap(),
-            ImageRetentionPolicy::DropWhenUnusedForOneFrame,
-        );
+        cx.set_image_loader(|cx, name|{
+            if name == "sample.png" {
+                // Load an image into the binary
+                cx.load_image(
+                    String::from("sample.png"),
+                    image::load_from_memory_with_format(
+                        include_bytes!("../resources/images/sample-hut-400x300.png"),
+                        image::ImageFormat::Png,
+                    )
+                    .unwrap(),
+                    ImageRetentionPolicy::DropWhenUnusedForOneFrame,
+                );
+            }
+        });
 
         cx.add_stylesheet(BACKGROUND_IMAGE_STYLE).expect("Failed to add stylesheet");
         cx.add_stylesheet(BACKGROUND_SIZE_STYLE).expect("Failed to add stylesheet");
+        cx.add_stylesheet(BORDER_STYLE).expect("Failed to add stylesheet");
 
         TabView::new(cx, StaticLens::new(STATIC_LIST.as_ref()), |cx, item| match item.get(cx) {
             "Background Image" => TabPair::new(
@@ -99,6 +270,26 @@ fn main() {
                 },
                 |cx| {
                     background_size(cx);
+                },
+            ),
+
+            "Border" => TabPair::new(
+                move |cx| {
+                    Label::new(cx, item).hoverable(false);
+                    Element::new(cx).class("indicator");
+                },
+                |cx| {
+                    border(cx);
+                },
+            ),
+
+            "Box Shadow" => TabPair::new(
+                move |cx| {
+                    Label::new(cx, item).hoverable(false);
+                    Element::new(cx).class("indicator");
+                },
+                |cx| {
+                    box_shadow(cx);
                 },
             ),
 
@@ -125,13 +316,40 @@ fn background_image(cx: &mut Context) {
 }
 
 fn background_size(cx: &mut Context) {
-
     VStack::new(cx, |cx|{
-        Element::new(cx).class("auto").class("element");
-        Element::new(cx).class("length").class("element");
-        Element::new(cx).class("percentage").class("element");
-        Element::new(cx).class("contain").class("element");
-        Element::new(cx).class("cover").class("element");
+        Element::new(cx).class("auto").class("image_element");
+        Element::new(cx).class("length").class("image_element");
+        Element::new(cx).class("percentage").class("image_element");
+        Element::new(cx).class("contain").class("image_element");
+        Element::new(cx).class("cover").class("image_element");
     }).class("container");
+}
 
+fn border(cx: &mut Context) {
+    HStack::new(cx, |cx| {
+        Element::new(cx).class("border").class("border_element");
+        Element::new(cx).class("border_radius").class("border_element");
+        Element::new(cx).class("border_shape").class("border_element");
+    })
+    .class("row");
+}
+
+fn box_shadow(cx: &mut Context) {
+    HStack::new(cx, |cx| {
+        Element::new(cx).class("shadow-offsetx");
+        Element::new(cx).class("shadow-offsety");
+        Element::new(cx).class("shadow-offset");
+        Element::new(cx).class("shadow-color");
+        Element::new(cx).class("shadow-blur");
+        Element::new(cx).class("shadow");
+    });
+
+    HStack::new(cx, |cx| {
+        Element::new(cx).class("shadow-inset-offsetx");
+        Element::new(cx).class("shadow-inset-offsety");
+        Element::new(cx).class("shadow-inset-offset");
+        Element::new(cx).class("shadow-inset-color");
+        Element::new(cx).class("shadow-inset-blur");
+        Element::new(cx).class("shadow-inset");
+    });
 }
