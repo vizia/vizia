6d7e43eb56bfe0de1d7b4b9d4259cacc175d90b1
George Atkinson <geomyles@yahoo.co.uk>
2023-07-04 22:57:06 +0100

    Change hover system to match web

--- crates/vizia_storage/src/tree/iter/mod.rs
+++ crates/vizia_storage/src/tree/iter/mod.rs
@@ -8,8 +8,13 @@ mod tree_iter;
 mod tree_tour_iter;
 
 pub use self::{
-    child_iter::ChildIterator, child_iter::MorphormChildIter, draw_iter::DrawIterator,
-    layout_child_iter::LayoutChildIterator, layout_tree_iter::LayoutTreeIterator,
-    parent_iter::ParentIterator, tree_depth_iter::TreeDepthIterator, tree_iter::TreeIterator,
+    child_iter::ChildIterator,
+    child_iter::MorphormChildIter,
+    draw_iter::DrawIterator,
+    layout_child_iter::LayoutChildIterator,
+    layout_tree_iter::LayoutTreeIterator,
+    parent_iter::{LayoutParentIterator, ParentIterator},
+    tree_depth_iter::TreeDepthIterator,
+    tree_iter::TreeIterator,
     tree_tour_iter::TreeTourIterator,
 };
