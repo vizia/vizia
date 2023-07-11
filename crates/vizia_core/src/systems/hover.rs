08e827cc59b02f28332898627f3da877116dc0de
George Atkinson <geomyles@yahoo.co.uk>
2023-07-11 11:37:41 +0100

    Fix for #368 and maybe #361

--- crates/vizia_core/src/systems/hover.rs
+++ crates/vizia_core/src/systems/hover.rs
@@ -48,7 +48,6 @@ pub(crate) fn hover_system(cx: &mut Context) {
                 && !pseudo_classes.contains(PseudoClassFlags::HOVER)
             {
                 pseudo_classes.set(PseudoClassFlags::HOVER, true);
-                cx.style.needs_restyle();
             }
         }
     }
@@ -164,6 +163,8 @@ fn hover_entity(
             {
                 if let Some(pseudo_class) = cx.style.pseudo_classes.get_mut(cx.current) {
                     pseudo_class.set(PseudoClassFlags::OVER, true);
+
+                    cx.needs_restyle();
                 }
             }
         } else if cx
@@ -176,6 +177,8 @@ fn hover_entity(
         {
             if let Some(pseudo_class) = cx.style.pseudo_classes.get_mut(cx.current) {
                 pseudo_class.set(PseudoClassFlags::OVER, false);
+
+                cx.needs_restyle();
             }
         }
     }
