b7ef08b23d364e182fa4a97eaf3482558dcee22f
Greatness7 <Greatness7@gmail.com>
2024-03-22 19:26:53 -0400

    Minor clean ups and optimizations to font/paragraph handling.

--- crates/vizia_core/src/systems/text.rs
+++ crates/vizia_core/src/systems/text.rs
@@ -1,9 +1,7 @@
-use skia_safe::font_arguments::variation_position::Coordinate;
-use skia_safe::font_arguments::VariationPosition;
-use skia_safe::textlayout::{
-    FontCollection, Paragraph, ParagraphBuilder, ParagraphStyle, TextStyle,
+use skia_safe::{
+    textlayout::{FontCollection, Paragraph, ParagraphBuilder, ParagraphStyle, TextStyle},
+    FontArguments, FontStyle, Paint,
 };
-use skia_safe::{FontArguments, FontStyle};
 use vizia_storage::LayoutTreeIterator;
 
 use crate::prelude::*;
@@ -73,79 +71,83 @@ pub fn build_paragraph(
     style: &Style,
     font_collection: &FontCollection,
 ) -> Option<Paragraph> {
-    style.text.get(entity).map(|text| {
-        let mut paragraph_style = ParagraphStyle::default();
-        if style.text_overflow.get(entity).copied().unwrap_or_default() == TextOverflow::Ellipsis {
-            paragraph_style.set_ellipsis("...");
+    let text = style.text.get(entity)?;
+
+    let mut paragraph_style = ParagraphStyle::default();
+    // paragraph_style.turn_hinting_off();
+
+    // Overflow
+    if style.text_overflow.get(entity) == Some(&TextOverflow::Ellipsis) {
+        paragraph_style.set_ellipsis("...");
+    }
+
+    // Line Clamp
+    if let Some(line_clamp) = style.line_clamp.get(entity) {
+        paragraph_style.set_max_lines(line_clamp.0 as usize);
+    }
+
+    // Text Align
+    paragraph_style.set_text_align(
+        if let Some(text_align) = style.text_align.get(entity) {
+            *text_align
+        } else if let Some(Units::Stretch(_)) = style.child_left.get(entity) {
+            if let Some(Units::Stretch(_)) = style.child_right.get(entity) {
+                TextAlign::Center
+            } else {
+                TextAlign::Right
+            }
+        } else {
+            TextAlign::Left
+        }
+        .into(),
+    );
+
+    // Text Style
+    paragraph_style.set_text_style(&{
+        let mut text_style = TextStyle::new();
+
+        // Font Families
+        text_style.set_font_families(
+            style
+                .font_family
+                .get(entity)
+                .map(Vec::as_slice)
+                .unwrap_or(&[FamilyOwned::Generic(GenericFontFamily::SansSerif)]),
+        );
+
+        // Font Color
+        if let Some(font_color) = style.font_color.get(entity) {
+            let mut paint = Paint::default();
+            paint.set_color(*font_color);
+            paint.set_anti_alias(true);
+            text_style.set_foreground_paint(&paint);
         }
 
-        paragraph_style.set_max_lines(style.line_clamp.get(entity).map(|lc| lc.0 as usize));
-        let text_align = if let Some(text_align) = style.text_align.get(entity).copied() {
-            text_align
-        } else {
-            let child_left = style.child_left.get(entity).copied().unwrap_or_default();
-            let child_right = style.child_right.get(entity).copied().unwrap_or_default();
-            if matches!(child_left, Units::Stretch(_)) {
-                if matches!(child_right, Units::Stretch(_)) {
-                    TextAlign::Center
-                } else {
-                    TextAlign::Right
-                }
-            } else {
-                TextAlign::Left
+        // Font Size
+        let font_size = style.font_size.get(entity).map_or(16.0, |f| f.0);
+        text_style.set_font_size(font_size.round() * style.scale_factor());
+
+        // Font Style
+        match (
+            style.font_weight.get(entity),
+            style.font_width.get(entity),
+            style.font_slant.get(entity),
+        ) {
+            (None, None, None) => {}
+            (weight, width, slant) => {
+                text_style.set_font_style(FontStyle::new(
+                    weight.copied().unwrap_or_default().into(),
+                    width.copied().unwrap_or_default().into(),
+                    slant.copied().unwrap_or_default().into(),
+                ));
             }
-        };
+        }
 
-        paragraph_style.set_text_align(text_align.into());
+        text_style
+    });
 
-        let mut text_style = TextStyle::new();
-        let font_weight = style.font_weight.get(entity).copied().unwrap_or_default();
-        let font_width = style.font_width.get(entity).copied().unwrap_or_default();
-        let font_slant = style.font_slant.get(entity).copied().unwrap_or_default();
-        let font_style = FontStyle::new(font_weight.into(), font_width.into(), font_slant.into());
-        text_style.set_font_style(font_style);
-        let font_families = style
-            .font_family
-            .get(entity)
-            .map(|families| {
-                families
-                    .iter()
-                    .map(|family| match family {
-                        FamilyOwned::Generic(generic) => match generic {
-                            _ => "Roboto Flex",
-                        },
-
-                        FamilyOwned::Named(name) => name.as_str(),
-                    })
-                    .collect()
-            })
-            .unwrap_or(vec!["Roboto Flex"]);
-
-        text_style.set_font_families(&font_families);
-
-        text_style.set_color(style.font_color.get(entity).copied().unwrap_or_default());
-
-        let font_size = style.font_size.get(entity).copied().map(|f| f.0).unwrap_or(16.0)
-            * style.scale_factor();
-        text_style.set_font_size(font_size);
-        // text_style.add_font_feature("opsz", 1);
-        let coordinates = Box::new([
-            Coordinate { axis: ('w', 'g', 'h', 't').into(), value: font_weight.0 as f32 },
-            Coordinate { axis: ('o', 'p', 's', 'z').into(), value: font_size },
-        ]);
-        let args = FontArguments::new();
-        let pos = VariationPosition { coordinates: coordinates.as_ref() };
-        let args = args.set_variation_design_position(pos);
-        text_style.set_font_arguments(&Some(args));
-
-        paragraph_style.turn_hinting_off();
-        paragraph_style.set_text_style(&text_style);
-        let mut paragraph_builder = ParagraphBuilder::new(&paragraph_style, font_collection);
-
-        paragraph_builder.push_style(&text_style);
-        paragraph_builder.add_text(text.as_str());
-        paragraph_builder.add_text("\u{200B}");
-
-        paragraph_builder.build()
-    })
+    let mut paragraph_builder = ParagraphBuilder::new(&paragraph_style, font_collection);
+    paragraph_builder.add_text(text.as_str());
+    paragraph_builder.add_text("\u{200B}");
+    paragraph_builder.build().into()
 }
