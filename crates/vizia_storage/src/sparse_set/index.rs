1c28f245ef7b5c8bf3ef7e53e9bf203a40b91755
Dr George Atkinson <geomyles@yahoo.co.uk>
2025-04-07 22:25:58 +0100

    Fix sparse set inheritance

--- crates/vizia_storage/src/sparse_set/index.rs
+++ crates/vizia_storage/src/sparse_set/index.rs
@@ -2,6 +2,9 @@ pub trait SparseSetIndex: PartialEq + Copy + Clone {
     fn new(index: usize) -> Self;
     fn null() -> Self;
     fn index(&self) -> usize;
+    fn is_null(&self) -> bool {
+        *self == Self::null()
+    }
 }
 
 impl SparseSetIndex for usize {
