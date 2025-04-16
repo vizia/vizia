0da26f3a47a7caafe1fb0652e83c0331452f2d8f
Dr George Atkinson <geomyles@yahoo.co.uk>
2025-04-16 17:14:30 +0100

    Prevent border from affecting layout

--- crates/vizia_core/src/layout/node.rs
+++ crates/vizia_core/src/layout/node.rs
@@ -317,40 +317,20 @@ impl Node for Entity {
         })
     }
 
-    fn border_left(&self, store: &Self::Store) -> Option<morphorm::Units> {
-        store.border_width.get(*self).map(|border_width| match border_width {
-            LengthOrPercentage::Length(val) => {
-                Units::Pixels(store.logical_to_physical(val.to_px().unwrap_or_default()))
-            }
-            LengthOrPercentage::Percentage(val) => Units::Percentage(*val),
-        })
+    fn border_left(&self, _store: &Self::Store) -> Option<morphorm::Units> {
+        None
     }
 
-    fn border_right(&self, store: &Self::Store) -> Option<morphorm::Units> {
-        store.border_width.get(*self).map(|border_width| match border_width {
-            LengthOrPercentage::Length(val) => {
-                Units::Pixels(store.logical_to_physical(val.to_px().unwrap_or_default()))
-            }
-            LengthOrPercentage::Percentage(val) => Units::Percentage(*val),
-        })
+    fn border_right(&self, _store: &Self::Store) -> Option<morphorm::Units> {
+        None
     }
 
-    fn border_top(&self, store: &Self::Store) -> Option<morphorm::Units> {
-        store.border_width.get(*self).map(|border_width| match border_width {
-            LengthOrPercentage::Length(val) => {
-                Units::Pixels(store.logical_to_physical(val.to_px().unwrap_or_default()))
-            }
-            LengthOrPercentage::Percentage(val) => Units::Percentage(*val),
-        })
+    fn border_top(&self, _store: &Self::Store) -> Option<morphorm::Units> {
+        None
     }
 
-    fn border_bottom(&self, store: &Self::Store) -> Option<morphorm::Units> {
-        store.border_width.get(*self).map(|border_width| match border_width {
-            LengthOrPercentage::Length(val) => {
-                Units::Pixels(store.logical_to_physical(val.to_px().unwrap_or_default()))
-            }
-            LengthOrPercentage::Percentage(val) => Units::Percentage(*val),
-        })
+    fn border_bottom(&self, _store: &Self::Store) -> Option<morphorm::Units> {
+        None
     }
 
     fn alignment(&self, store: &Self::Store) -> Option<morphorm::Alignment> {
