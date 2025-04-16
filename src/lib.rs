51b9fc55401b87e6ed60fe687ac211ada43ef430
Dr George Atkinson <geomyles@yahoo.co.uk>
2025-04-16 16:34:17 +0100

    Remove baseview feature for publish

--- src/lib.rs
+++ src/lib.rs
@@ -4,27 +4,19 @@
 
 extern crate self as vizia;
 
-#[cfg(all(not(feature = "baseview"), feature = "winit"))]
+#[cfg(feature = "winit")]
 pub use vizia_winit::application::{Application, ApplicationError};
-
-#[cfg(all(not(feature = "winit"), feature = "baseview"))]
-pub use vizia_baseview::{
-    Application, ApplicationError, ParentWindow, WindowHandle, WindowScalePolicy,
-};
 
 pub use vizia_core::*;
 
 pub mod prelude {
     pub use vizia_core::prelude::*;
 
-    #[cfg(all(not(feature = "baseview"), feature = "winit"))]
+    #[cfg(feature = "winit")]
     pub use vizia_winit::{
         ModifyWindow,
         application::{Application, ApplicationError},
         window::Window,
         window_modifiers::WindowModifiers,
     };
-
-    #[cfg(all(not(feature = "winit"), feature = "baseview"))]
-    pub use vizia_baseview::{Application, ApplicationError, WindowHandle, WindowScalePolicy};
 }
