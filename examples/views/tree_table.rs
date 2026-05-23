7cb912821477937284d619722959a0d04b90ce93
George Atkinson <geomyles@yahoo.co.uk>
2026-05-23 17:31:03 +0100

    Fix warnings in examples

--- examples/views/tree_table.rs
+++ examples/views/tree_table.rs
@@ -506,7 +506,7 @@ fn main() -> Result<(), ApplicationError> {
                 cx,
                 tree,
                 cols,
-                { move |tree: &Tree<FsNode>| vec![tree.root().id()] },
+                move |tree: &Tree<FsNode>| vec![tree.root().id()],
                 {
                     let sort_state = sort_state;
                     move |tree: &Tree<FsNode>, parent_id: &NodeId| {
