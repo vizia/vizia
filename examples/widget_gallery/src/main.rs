935574d44b0d27b5ae9df98dc6af65bdb4c9cc7f
albi <albi@fedodo.com>
2026-08-19 23:58:21 +0200

    ui: use progressive animation card throbber

--- examples/widget_gallery/src/main.rs
+++ examples/widget_gallery/src/main.rs
@@ -361,7 +361,7 @@ struct PreviewTableRow {
 fn render_view_preview(cx: &mut Context, view_name: &'static str) {
     VStack::new(cx, |cx| match view_name {
         "Animation" => {
-            TextThrobber::with_indicator(cx, "Vizia UI", TextThrobberIndicator::Dots);
+            TextThrobber::new(cx, "Vizia UI", TextThrobberVariant::Progressive);
         }
         "Accordion" => {
             let items = Signal::new(vec![
